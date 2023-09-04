6d654e9ef82b2f4dd8e2d21b216b661c74a62c6e
KIMGEUNDU <vlkoklv@naver.com>
2023-09-04 16:31:54 +0900

    [생성] develop 브랜치

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,5 @@
-import { useBearsStore } from './store/bears';
-import AddBearButton from './components/AddBearButton';
-
 function App() {
-  const bears = useBearsStore((state) => state.bears);
-  console.log(bears);
-
-  return (
-    <div>
-      {bears.length}
-      <AddBearButton />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
