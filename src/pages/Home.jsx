3dadfc571539ee5587023308f5f06d1da28a0286
KIMGEUNDU <vlkoklv@naver.com>
2023-09-06 01:04:29 +0900

    [추가]  gsap 애니메이션 효과

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,33 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
+import gsap from 'gsap';
 
 function Home() {
 	const navigate = useNavigate();
+	const containerRef = useRef(null);
+
 	useEffect(() => {
-		const time = setTimeout(() => {
-			navigate('/login');
-		}, 2500);
+		const timeline = gsap.timeline();
+
+		timeline.to(containerRef.current, {
+			opacity: 0,
+			duration: 1,
+			delay: 2,
+			onComplete: () => {
+				navigate('/login');
+			},
+		});
 
 		return () => {
-			clearTimeout(time);
+			timeline.kill();
 		};
 	}, [navigate]);
 
 	return (
-		<div className="max-w-[600px] min-w-[320px] bg-ec4 text-ec1 flex flex-row-reverse justify-center items-center h-screen m-auto">
+		<div
+			ref={containerRef}
+			className="max-w-[600px] min-w-[320px] bg-ec4 text-ec1 flex flex-row-reverse justify-center items-center h-screen m-auto"
+		>
 			<h1 className="font-hob text-6xl">방탈러</h1>
 			<img
 				className="h-[90px] mr-5"
