f160e47a633fddab5e91ab7683d35d11964bc21f
KIMGEUNDU <vlkoklv@naver.com>
2023-09-05 19:49:23 +0900

    [수정] App.jsx 라우터 코드 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import { RouterProvider } from 'react-router-dom';
+import router from './routes';
+
 function App() {
-  return <div>
-    <header></header>
-  </div>;
+	return <RouterProvider router={router} />;
 }
 
 export default App;
