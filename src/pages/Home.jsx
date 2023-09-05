3ae6ec4864983f3e7b655c3b83aa0eee9f0528c6
KIMGEUNDU <vlkoklv@naver.com>
2023-09-06 00:57:38 +0900

    [수정] Home.jsx 코드 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,18 +3,27 @@ import { useNavigate } from 'react-router-dom';
 
 function Home() {
 	const navigate = useNavigate();
-
 	useEffect(() => {
 		const time = setTimeout(() => {
 			navigate('/login');
-		}, 1000);
+		}, 2500);
 
 		return () => {
 			clearTimeout(time);
 		};
 	}, [navigate]);
 
-	return <div>Home</div>;
+	return (
+		<div className="max-w-[600px] min-w-[320px] bg-ec4 text-ec1 flex flex-row-reverse justify-center items-center h-screen m-auto">
+			<h1 className="font-hob text-6xl">방탈러</h1>
+			<img
+				className="h-[90px] mr-5"
+				src="/로고열쇠.svg"
+				alt="방탈러로고이미지"
+				aria-hidden
+			/>
+		</div>
+	);
 }
 
 export default Home;
