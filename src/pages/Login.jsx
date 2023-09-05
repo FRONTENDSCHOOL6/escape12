5055874f0722c949e64ba4dee2e761a1110081d8
KIMGEUNDU <vlkoklv@naver.com>
2023-09-06 03:31:08 +0900

    [수정] 마크업

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,22 @@
-import PlusButton from '@/components/PlusButton';
+import Logo from '@/components/Logo';
+import { Helmet } from 'react-helmet-async';
 
 function Login() {
 	return (
-		<div>
-			<PlusButton />
-		</div>
+		<>
+			<Helmet>
+				<title>방탈러-로그인</title>
+			</Helmet>
+			<div className="max-w-[600px] min-w-[320px] bg-ec4 text-ec1 flex flex-col justify-around items-center h-screen m-auto">
+				<Logo width={80} />
+				<form action="">
+					<fieldset>
+						<label htmlFor="">아이디/이메일</label>
+						<input type="email" />
+					</fieldset>
+				</form>
+			</div>
+		</>
 	);
 }
 
