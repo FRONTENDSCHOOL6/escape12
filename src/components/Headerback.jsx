9dd96183a8e0a3118d4c32a2a10cb73c5c299e1a
JOSuYeoM <syjo1227@naver.com>
2023-09-06 21:39:12 +0900

    [해결] 뒤로가기 버튼 위치 조정 완료

--- src/components/Headerback.jsx
+++ src/components/Headerback.jsx
@@ -8,9 +8,8 @@ Headerback.propTypes = {
 function Headerback({ children }) {
   return (
     <div>
-      <header className="max-w-[600px] min-w-[600px] py-3 text-center bg-ec4 text-ec1 text-2xl font-bold relative leading-2">
+      <header className="max-w-[600px] min-w-[600px] py-3 text-center bg-ec4 text-ec1 text-2xl font-bold relative leading-2 flex items-center">
         <button className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-transparent border-none cursor-pointer">&lt;</button>
-
         <div className="mx-auto">{children}</div>
       </header>
     </div>
