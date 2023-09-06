8e4cb57f7195eccc4b9fa0ff6d7af03218d76b4f
skawls <ventu9204@gmail.com>
2023-09-06 17:47:51 +0900

    [추가] 포스트페이지 라우트 링크 추가

--- src/routes.jsx
+++ src/routes.jsx
@@ -6,12 +6,14 @@ import {
 import Empty from '@/layout/Empty';
 import Home from '@/pages/Home';
 import Login from './pages/Login';
+import PostPage from './pages/PostPage';
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path="/" element={<Empty />}>
 			<Route index element={<Home />} />
 			<Route path="/login" element={<Login />} />
+			<Route path="/PostPage" element={<PostPage />} />
 		</Route>
 	)
 );
