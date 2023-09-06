d523e728f4e8d963903d21ab9a80e382b4e3b4a3
JOSuYeoM <syjo1227@naver.com>
2023-09-06 16:51:44 +0900

    [수정] 파일명 수정

--- src/routes.jsx
+++ src/routes.jsx
@@ -7,7 +7,6 @@ import Empty from '@/layout/Empty';
 import Home from '@/pages/Home';
 import Login from './pages/Login';
 import Mypage from './pages/Mypage';
-import Changepage from './pages/Changepage';
 import Editpage from './pages/Editpage';
 
 const router = createBrowserRouter(
@@ -16,7 +15,6 @@ const router = createBrowserRouter(
 			<Route index element={<Home />} />
 			<Route path="/login" element={<Login />} />
 			<Route path="/mypage" element={<Mypage />} />
-			<Route path="/changepage" element={<Changepage />} />
 			<Route path="/editpage" element={<Editpage />} />
 
 		</Route>
