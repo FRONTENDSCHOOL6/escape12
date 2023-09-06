7cc288328381f08d841df149959266416aa29edc
KIMGEUNDU <vlkoklv@naver.com>
2023-09-06 15:50:19 +0900

    [추가] LoginSelete 컴포넌트 path 추가

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,12 +5,14 @@ import {
 } from 'react-router-dom';
 import Empty from '@/layout/Empty';
 import Home from '@/pages/Home';
-import Login from './pages/Login';
+import Login from '@/pages/Login';
+import LoginSelete from './pages/LoginSelete';
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path="/" element={<Empty />}>
 			<Route index element={<Home />} />
+			<Route path="/loginselete" element={<LoginSelete />} />
 			<Route path="/login" element={<Login />} />
 		</Route>
 	)
