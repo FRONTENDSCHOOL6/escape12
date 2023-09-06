73ed5c87eafb0763992d6c89c54718588739184d
KIMGEUNDU <vlkoklv@naver.com>
2023-09-07 01:57:57 +0900

    [추가] onClick 이벤트 추가

--- src/components/LoginSeleteButton.jsx
+++ src/components/LoginSeleteButton.jsx
@@ -1,13 +1,14 @@
-import { string } from 'prop-types';
+import { string, func } from 'prop-types';
 
 LoginSeleteButton.propTypes = {
 	children: string.isRequired,
 	line: string.isRequired,
 	text: string.isRequired,
 	img: string.isRequired,
+	onClick: func,
 };
 
-function LoginSeleteButton({ children, line, text, img }) {
+function LoginSeleteButton({ children, line, text, img, onClick = '' }) {
 	const buttonLeft = {
 		backgroundPosition: '10% 50%',
 	};
@@ -15,6 +16,7 @@ function LoginSeleteButton({ children, line, text, img }) {
 	return (
 		<button
 			type="button"
+			onClick={onClick}
 			className={`text-center rounded-3xl border-2 ${line} font-semibold ${text} px-32 py-3 ${img} bg-no-repeat`}
 			style={buttonLeft}
 		>
