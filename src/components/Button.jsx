3a6efb7419da37dc4a5a5686fb4ca68b74402ed9
KIMGEUNDU <vlkoklv@naver.com>
2023-09-06 17:34:07 +0900

    [수정] Button 컴포넌트 수정 및 props 추가

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,21 +1,31 @@
 import { Link } from 'react-router-dom';
-import  { string } from 'prop-types';
+import { string } from 'prop-types';
 
 Button.propTypes = {
-  path: string.isRequired,
-  children: string.isRequired,
+	path: string.isRequired,
+	type: string.isRequired,
+	bg: string.isRequired,
+	text: string.isRequired,
+	children: string.isRequired,
 };
 
-function Button({ path, children}) {
-  return (
-    <Link to={path}>
-      <button
-        type="button"
-        className="text-center rounded-lg bg-ec3 text-ec1 w-32 h-8 font-semibold text-sm">
-        {children}
-      </button>
-    </Link>
-  );
+function Button({
+	path = '',
+	type = 'button',
+	bg = 'bg-ec3',
+	text = 'bg-ec1',
+	children,
+}) {
+	return (
+		<Link to={path}>
+			<button
+				type={type}
+				className={`text-center rounded-lg ${bg} ${text} w-32 h-8 font-semibold`}
+			>
+				{children}
+			</button>
+		</Link>
+	);
 }
 
 export default Button;
