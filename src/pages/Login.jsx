5c2c24620050fb145c8c52db2caf0d12661b6eac
KIMGEUNDU <vlkoklv@naver.com>
2023-09-06 17:31:17 +0900

    [수정] Login 컴포넌트 수정중

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,8 @@
-import Logo from '@/components/Logo';
+import Button from '@/components/Button';
+import FormInput from '@/components/FormInput';
+import KeyLogo from '@/components/KeyLogo';
 import { Helmet } from 'react-helmet-async';
+import { Link } from 'react-router-dom';
 
 function Login() {
 	return (
@@ -7,14 +10,36 @@ function Login() {
 			<Helmet>
 				<title>방탈러-로그인</title>
 			</Helmet>
-			<div className="max-w-[600px] min-w-[320px] bg-ec4 text-ec1 flex flex-col justify-around items-center h-screen m-auto">
-				<Logo width={80} />
-				<form action="">
-					<fieldset>
-						<label htmlFor="">아이디/이메일</label>
-						<input type="email" />
+			<div className="max-w-[600px] min-w-[320px] bg-ec4 flex flex-col items-center h-screen m-auto text-lg gap-10">
+				<KeyLogo />
+				<form action="" className="flex flex-col gap-10 items-center">
+					<fieldset className="flex flex-col gap-8">
+						<FormInput type="email" name="id">
+							아이디/이메일
+						</FormInput>
+						<FormInput type="password" name="password">
+							비밀번호
+						</FormInput>
 					</fieldset>
+					<Button type="submit" path="/" bg="bg-ec1">
+						로그인
+					</Button>
 				</form>
+				<Link to="">
+					<button type="button" className="text-ec1">
+						아이디/비밀번호 찾기
+					</button>
+				</Link>
+				<Link to="">
+					<button type="button" className="text-ec1">
+						회원가입
+					</button>
+				</Link>
+				<Link to="/loginselete">
+					<button type="button" className="text-ec1">
+						뒤로가기
+					</button>
+				</Link>
 			</div>
 		</>
 	);
