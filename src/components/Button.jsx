51dc851f7f374f8ad3083868c3308e1898c0f434
KIMGEUNDU <vlkoklv@naver.com>
2023-09-08 00:01:09 +0900

    [추가] Button 컴포넌트 onClick 추가

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,25 @@
-import { string } from 'prop-types';
+import { string, func } from 'prop-types';
 
 Button.propTypes = {
 	type: string,
 	bg: string,
 	text: string,
 	children: string,
+	onClick: func,
 };
 
-function Button({ type = 'button', bg = 'bg-ec3', text = 'bg-ec1', children }) {
+function Button({
+	type = 'button',
+	bg = 'bg-ec3',
+	text = 'bg-ec1',
+	onClick = null,
+	children,
+}) {
 	return (
 		<button
 			type={type}
 			className={`text-center rounded-lg ${bg} ${text} w-32 h-8 font-semibold`}
+			onClick={onClick}
 		>
 			{children}
 		</button>
