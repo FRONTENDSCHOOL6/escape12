fdbf68aed5e410c006216449e97b378ad33d0e96
JOSuYeoM <syjo1227@naver.com>
2023-09-07 23:49:12 +0900

    [추가] screens 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,30 +2,32 @@
 export default {
 	content: ['./index.html', './src/**/*.jsx'],
 	theme: {
-		fontFamily: {
-			hob: ['SDSamliphopangche_Outline'],
-		},
-		screens: {
-			width: { min: '320px', max: '600px' },
-		},
-		colors: {
-			ec1: '#FAF0E6',
-			ec2: '#B9B4C7',
-			ec3: '#5C5470',
-			ec4: '#352F44',
-			ec5: '#F8DE22',
-			red: '#FA0000',
-			kakaoline: '#FFE895',
-			googleline: '#BCF0AB',
-		},
-		backgroundImage: {
-			kakao: 'url("/src/assets/loginselete-kakao.png")',
-			google: 'url("/src/assets/loginselete-google.png")',
-			key: 'url("/src/assets/loginselete-key.png")',
-			eyetrue: 'url("/src/assets/login-eyetrue.png.png")',
-			eyefalse: 'url("/src/assets/login-eyefalse.png.png")',
-		},
-		extend: {},
+			fontFamily: {
+					hob: ['SDSamliphopangche_Outline'],
+			},
+			screens: {
+					s: { min: '320px', max: '600px' },
+					m: { min: '601px', max: '1279px' },
+					l: { min: '1280px' },
+			},
+			colors: {
+					ec1: '#FAF0E6',
+					ec2: '#B9B4C7',
+					ec3: '#5C5470',
+					ec4: '#352F44',
+					ec5: '#F8DE22',
+					red: '#FA0000',
+					kakaoline: '#FFE895',
+					googleline: '#BCF0AB',
+			},
+			backgroundImage: {
+					kakao: 'url("/src/assets/loginselete-kakao.png")',
+					google: 'url("/src/assets/loginselete-google.png")',
+					key: 'url("/src/assets/loginselete-key.png")',
+					eyetrue: 'url("/src/assets/login-eyetrue.png.png")',
+					eyefalse: 'url("/src/assets/login-eyefalse.png.png")',
+			},
+			extend: {},
 	},
 	plugins: [],
 };
