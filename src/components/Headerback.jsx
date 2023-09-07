38b513133381739ff8ea6fbfbaf689f24dd1d95e
JOSuYeoM <syjo1227@naver.com>
2023-09-07 17:11:35 +0900

    [수정] 코드 수정

--- src/components/Headerback.jsx
+++ src/components/Headerback.jsx
@@ -2,19 +2,21 @@ import { string } from 'prop-types';
 import { Link } from 'react-router-dom';
 
 Headerback.propTypes = {
-  path: string.isRequired,
-  children: string.isRequired,
+  path: string,
+  children: string,
 };
 
 function Headerback({ path = "", children }) {
   return (
     <div>
       <header className="py-3 text-center bg-ec4 text-ec1 text-2xl sm:text-xl font-bold leading-2">
-        <div className="relative">
+        <div className="absolute left-4 top-7 mx-auto max-w-screen-md flex">
           <Link to={path}>
-            <button className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-transparent border-none cursor-pointer">&lt;</button>
+            <button className='transform -translate-y-1/2 bg-transparent border-none '>
+              &lt;
+            </button>
           </Link>
-          <div className="mx-auto max-w-screen-md">{children}</div>
+          <div className="mx-auto">{children}</div>
         </div>
       </header>
     </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,52 +1,52 @@
 import { NavLink } from 'react-router-dom';
 
 function Nav() {
-	return (
-		<nav className="max-w-[600px] min-w-[600px] text-center bg-ec3 text-ec1 text-base font-bold">
-			<ul className="flex justify-around items-center py-5">
-				<li className={'hover:text-ec5'}>
-					<NavLink
-						to="theme"
-						className={({ isActive }) => {
-							isActive ? 'text-ec1' : 'text-ec5';
-						}}
-					>
-						테마
-					</NavLink>
-				</li>
-				<li className={'hover:text-ec5'}>
-					<NavLink
-						to="record"
-						className={({ isActive }) => {
-							isActive ? 'text-ec1' : 'text-ec5';
-						}}
-					>
-						기록
-					</NavLink>
-				</li>
-				<li className={'hover:text-ec5'}>
-					<NavLink
-						to="community"
-						className={({ isActive }) => {
-							isActive ? 'text-ec1' : 'text-ec5';
-						}}
-					>
-						커뮤니티
-					</NavLink>
-				</li>
-				<li className={'hover:text-ec5'}>
-					<NavLink
-						to="my"
-						className={({ isActive }) => {
-							isActive ? 'text-ec1' : 'text-ec5';
-						}}
-					>
-						마이
-					</NavLink>
-				</li>
-			</ul>
-		</nav>
-	);
+  return (
+    <nav className="text-center bg-ec3 text-ec1 text-base font-bold">
+      <ul className="flex justify-around items-center py-5">
+        <li className="hover:text-ec5">
+          <NavLink
+            to="theme"
+            className={({ isActive }) => {
+              isActive ? 'text-ec1' : 'text-ec5';
+            }}
+          >
+            테마
+          </NavLink>
+        </li>
+        <li className="hover:text-ec5">
+          <NavLink
+            to="record"
+            className={({ isActive }) => {
+              isActive ? 'text-ec1' : 'text-ec5';
+            }}
+          >
+            기록
+          </NavLink>
+        </li>
+        <li className="hover:text-ec5">
+          <NavLink
+            to="community"
+            className={({ isActive }) => {
+              isActive ? 'text-ec1' : 'text-ec5';
+            }}
+          >
+            커뮤니티
+          </NavLink>
+        </li>
+        <li className="hover:text-ec5">
+          <NavLink
+            to="my"
+            className={({ isActive }) => {
+              isActive ? 'text-ec1' : 'text-ec5';
+            }}
+          >
+            마이
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Nav;
