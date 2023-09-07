79fc28d3145d828e1ba80b5056b5808f879933fd
JOSuYeoM <syjo1227@naver.com>
2023-09-07 09:43:32 +0900

    [추가] 버튼 클릭시 경로 이동 코드 추가

--- src/components/Headerback.jsx
+++ src/components/Headerback.jsx
@@ -1,15 +1,21 @@
 import { string } from 'prop-types';
-
+import { Link } from 'react-router-dom';
 
 Headerback.propTypes = {
+  path: string.isRequired,
   children: string.isRequired,
 };
 
-function Headerback({ children }) {
+function Headerback({
+	path = "",
+	children,
+}) {
   return (
     <div>
       <header className="max-w-[600px] min-w-[600px] py-3 text-center bg-ec4 text-ec1 text-2xl font-bold relative leading-2 flex items-center">
-        <button className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-transparent border-none cursor-pointer">&lt;</button>
+        <Link to={path}>
+          <button className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-transparent border-none cursor-pointer">&lt;</button>
+        </Link>
         <div className="mx-auto">{children}</div>
       </header>
     </div>
--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -9,8 +9,6 @@ function Mypage() {
       <Helmet>
         <title>마이페이지</title>
       </Helmet>
-
-      {/* 진짜 페이지 부분 */}
       <div className="w-full max-w-[600px] min-w-[320px] bg-ec4 text-ec1 flex flex-col items-center h-screen m-auto gap-14">
         <Header>마이페이지</Header>
         <div className='p-3 text-xl'>
@@ -18,8 +16,8 @@ function Mypage() {
           <p className="p-3">비밀번호| ********</p>
           <p className="p-3">닉네임| </p>
         </div>
-        <Button bg="bg-ec1" text="text-ec4">정보수정</Button>
-        <div className="border-2 p-6 text-lg rounded-xl">
+          <Button path="/editpage" bg="bg-ec1" text="text-ec4">정보수정</Button>
+        <div className="border-2 p-9 text-lg rounded-xl">
           <p className="p-3">내가 작성한 기록 12조 최고 ~ </p>
           <p className="p-3">내가 작성한 글 ********</p>
           <p className="p-3">내가 작성한 댓글 </p>
