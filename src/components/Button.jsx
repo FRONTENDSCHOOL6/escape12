108cb604a16a3e8ed1535227ae38e7f7e03b04da
KIMGEUNDU <vlkoklv@naver.com>
2023-09-07 20:34:17 +0900

    [수정] 문법 오류 코드 수정

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,30 +1,20 @@
-import { Link } from 'react-router-dom';
 import { string } from 'prop-types';
 
 Button.propTypes = {
-	path: string,
 	type: string,
 	bg: string,
 	text: string,
 	children: string,
 };
 
-function Button({
-	path = '',
-	type = 'button',
-	bg = 'bg-ec3',
-	text = 'bg-ec1',
-	children,
-}) {
+function Button({ type = 'button', bg = 'bg-ec3', text = 'bg-ec1', children }) {
 	return (
-		<Link to={path}>
-			<button
-				type={type}
-				className={`text-center rounded-lg ${bg} ${text} w-32 h-8 font-semibold`}
-			>
-				{children}
-			</button>
-		</Link>
+		<button
+			type={type}
+			className={`text-center rounded-lg ${bg} ${text} w-32 h-8 font-semibold`}
+		>
+			{children}
+		</button>
 	);
 }
 
