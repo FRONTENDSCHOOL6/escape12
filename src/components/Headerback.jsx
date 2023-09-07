68703c660506fb8c4253221da6b7b7cfff85f4cb
JOSuYeoM <syjo1227@naver.com>
2023-09-07 16:09:28 +0900

    [임시완료] 뒤로가기 컴포넌트 임시완료

--- src/components/Headerback.jsx
+++ src/components/Headerback.jsx
@@ -6,22 +6,19 @@ Headerback.propTypes = {
   children: string.isRequired,
 };
 
-function Headerback({
-	path = "",
-	children,
-}) {
+function Headerback({ path = "", children }) {
   return (
     <div>
-      <header className="max-w-[600px] min-w-[600px] py-3 text-center bg-ec4 text-ec1 text-2xl font-bold relative leading-2 flex items-center">
-        <Link to={path}>
-          <button className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-transparent border-none cursor-pointer">&lt;</button>
-        </Link>
-        <div className="mx-auto">{children}</div>
+      <header className="py-3 text-center bg-ec4 text-ec1 text-2xl sm:text-xl font-bold leading-2">
+        <div className="relative">
+          <Link to={path}>
+            <button className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-transparent border-none cursor-pointer">&lt;</button>
+          </Link>
+          <div className="mx-auto max-w-screen-md">{children}</div>
+        </div>
       </header>
     </div>
   );
 }
-
-
 
 export default Headerback;
