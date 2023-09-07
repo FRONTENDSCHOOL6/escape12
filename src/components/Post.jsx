2933d105142095fe122ea5794f3019715efa5de1
skawls <ventu9204@gmail.com>
2023-09-08 01:03:07 +0900

    [수정]  포스트 컴포넌트 핸들러 삭제

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,33 +1,39 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 Post.propTypes = {
 	title: PropTypes.string.isRequired,
 	author: PropTypes.string.isRequired,
 	date: PropTypes.string.isRequired,
-	onDelete: PropTypes.func.isRequired,
+	id: PropTypes.string.isRequired,
+	// onDelete: PropTypes.func.isRequired,
 };
 
-function Post({ title, author, date, onDelete }) {
-	const [isClicked, setIsClicked] = useState(true);
-	const handleDelete = () => {
-		onDelete();
-	};
+function Post({ id, title, author, date /*onDelete*/ }) {
+	// const [isClicked, setIsClicked] = useState(false);
+	// const handleDelete = () => {
+	// 	onDelete();
+	// };
 
 	return (
-		<div className="post max-w-[600px] min-w-[320px] bg-ec4 text-ec1 flex justify-around items-center h-screen m-auto">
-			<h2 className="post-title" onClick={() => setIsClicked(!isClicked)}>
-				{title}
-			</h2>
-			<p className="post-author">{author}</p>
-			<p className="post-date">{date}</p>
-			{isClicked && <p className="">게시글 내용 작성.재밌어요!</p>}
-			<button
-				onClick={handleDelete}
-				className="px-2 py-1 bg-ec3 text-ec1 rounded hover:text-ec5"
-			>
-				삭제
-			</button>
+		<div className="post max-w-[600px] min-w-[320px] bg-ec1 text-ec4 p-4 mb-4 rounded border flex flex-col justify-between ">
+			<div className="flex justify-between items-center">
+				<Link to={`/post/${id}`} className="post-title">
+					{title}
+				</Link>
+				<button
+					// onClick={handleDelete}
+					className="px-2 py-1 bg-ec3 text-ec1 rounded hover:text-ec5"
+				>
+					삭제
+				</button>
+			</div>
+			{/* {isClicked && <p className="">.재밌어요!</p>} */}
+			<div className="flex justify-between items-center mt-2">
+				<p className="post-author text-ec4 mb-1 mt-2">{author}</p>
+				<p className="post-date text-ec4">{date}</p>
+			</div>
 		</div>
 	);
 }
