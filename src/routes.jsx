b2d978a3d148720aad0ded1ccdfc348e7bb421dd
KIMGEUNDU <vlkoklv@naver.com>
2023-09-07 15:21:56 +0900

    [추가] signup 라우터 추가

--- src/routes.jsx
+++ src/routes.jsx
@@ -10,6 +10,7 @@ import LoginSelete from './pages/LoginSelete';
 import Mypage from './pages/MyPage';
 import Editpage from './pages/EditPage';
 import PostPage from './pages/PostPage';
+import SignUp from './pages/SignUp';
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
@@ -17,6 +18,7 @@ const router = createBrowserRouter(
 			<Route index element={<Home />} />
 			<Route path="/loginselete" element={<LoginSelete />} />
 			<Route path="/login" element={<Login />} />
+			<Route path="/signup" element={<SignUp />} />
 			<Route path="/mypage" element={<Mypage />} />
 			<Route path="/editpage" element={<Editpage />} />
 			<Route path="/PostPage" element={<PostPage />} />
