35ffef94f29308b7ce38663bba8a5128e65d66e2
KIMGEUNDU <vlkoklv@naver.com>
2023-09-07 14:14:49 +0900

    [수정] FormInputValid 스타일 수정

--- src/components/FormInputValid.jsx
+++ src/components/FormInputValid.jsx
@@ -4,7 +4,7 @@ FormInputValid.propTypes = {
 	children: string.isRequired,
 };
 function FormInputValid({ children }) {
-	return <p className="text-red mb-4 text-right">{children}</p>;
+	return <p className="text-red text-right">{children}</p>;
 }
 
 export default FormInputValid;
