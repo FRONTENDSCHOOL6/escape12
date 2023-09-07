af282714173eb25fbd745d600c4134eb50543d67
KIMGEUNDU <vlkoklv@naver.com>
2023-09-07 20:32:54 +0900

    [수정] 높이 추가 및 스타일 변경

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -10,13 +10,13 @@ FormInput.propTypes = {
 function FormInput({ children, type, name, onChange = null, ...restProps }) {
 	return (
 		<>
-			<fieldset className="flex gap-5 justify-between">
-				<label htmlFor={name} className="text-ec1 w-32">
+			<fieldset className="flex gap-5 justify-between text-ec1">
+				<label htmlFor={name} className="w-32">
 					{children}
 				</label>
 				<input
 					type={type}
-					className="w-[300px] pl-2"
+					className="w-[300px] pl-2 bg-ec4 border-b-2 border-ec1 focus:outline-none"
 					name={name}
 					id={name}
 					onChange={onChange}
