081d4ba6365670a08f96fe90d069c30250139313
JOSuYeoM <syjo1227@naver.com>
2023-09-08 10:03:12 +0900

    [수정] 코드 일부 수정, ""->''로 수정

--- src/components/Headerback.jsx
+++ src/components/Headerback.jsx
@@ -1,22 +1,25 @@
-import { string } from 'prop-types';
-import { Link } from 'react-router-dom';
+import { string, func } from 'prop-types';
 
 Headerback.propTypes = {
-  path: string,
+
   children: string,
+  onClick: func,
 };
 
-function Headerback({ path = "", children }) {
+function Headerback({
+  onClick = '',
+  children,
+}) {
   return (
     <div>
-      <header className="py-3 text-center bg-ec4 text-ec1 text-2xl sm:text-xl font-bold leading-2">
-        <div className="max-w-screen-md mx-auto flex items-center">
-          <Link to={path}>
-            <button className='ml-2'>
-              &lt;
-            </button>
-          </Link>
-          <div className="mx-auto">{children}</div>
+      <header className='py-3 text-center bg-ec4 text-ec1 text-2xl font-bold leading-2'>
+        <div className='mx-auto flex items-center'>
+          <button
+            className='ml-2'
+            onClick={onClick}  >
+            &lt;
+          </button>
+          <div className='mx-auto'>{children}</div>
         </div>
       </header>
     </div>
