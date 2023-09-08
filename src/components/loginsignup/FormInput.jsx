4af8ca43d27332b1112ad9fa1ed3a33df93930ef
KIMGEUNDU <vlkoklv@naver.com>
2023-09-09 02:46:08 +0900

    [수정] FormInput 디자인 수정

--- src/components/loginsignup/FormInput.jsx
+++ src/components/loginsignup/FormInput.jsx
@@ -3,26 +3,41 @@ import { string, func } from 'prop-types';
 FormInput.propTypes = {
 	type: string,
 	name: string,
+	bg: string,
 	children: string,
 	onChange: func,
+	onClick: func,
 };
 
-function FormInput({ children, type, name, onChange = null, ...restProps }) {
+function FormInput({
+	children,
+	type,
+	name,
+	bg = '',
+	onChange = null,
+	onClick = null,
+	...restProps
+}) {
 	return (
 		<>
-			<fieldset className="flex gap-5 justify-between text-ec1">
+			<fieldset className="flex gap-5 justify-between text-ec1 relative">
 				<label htmlFor={name} className="w-32 s:w-28">
 					{children}
 				</label>
 				<input
 					type={type}
-					className="w-[300px] s:w-[200px] bg-ec4 border-b-2 border-ec1 focus:outline-none webkit"
+					className="w-[300px] s:w-[200px] bg-opacity border-b-2 border-ec1 focus:outline-none"
 					name={name}
 					id={name}
 					onChange={onChange}
 					required
 					autoComplete="off"
 					{...restProps}
+				/>
+				<button
+					type="button"
+					className={`${bg} bg-cover w-5 h-5 absolute right-0 top-0`}
+					onClick={onClick}
 				/>
 			</fieldset>
 		</>
