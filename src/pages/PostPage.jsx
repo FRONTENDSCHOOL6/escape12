f75b84d0d4c32e2f3636d6392c209c15191e8312
skawls <ventu9204@gmail.com>
2023-09-08 15:00:42 +0900

    [수정] 더미데이터 연결 및 수정

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -29,6 +29,78 @@ function PostPage() {
 			content:
 				'긴 게시글 만들기긴 게시글 만들기긴 게시글 ssssssssssssssssssssssssssssssssssssssssssssssssssssssssss만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기 긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기',
 		},
+		{
+			id: 2,
+			title: '방탈출 후기입니다!',
+			author: '건주',
+			date: '2023-09-03',
+			content:
+				'긴 게시글 만들기긴 게시글 만들기긴 게시글 ssssssssssssssssssssssssssssssssssssssssssssssssssssssssss만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기 긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기',
+		},
+		{
+			id: 2,
+			title: '방탈출 후기입니다!',
+			author: '건주',
+			date: '2023-09-03',
+			content:
+				'긴 게시글 만들기긴 게시글 만들기긴 게시글 ssssssssssssssssssssssssssssssssssssssssssssssssssssssssss만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기 긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기',
+		},
+		{
+			id: 2,
+			title: '방탈출 후기입니다!',
+			author: '건주',
+			date: '2023-09-03',
+			content:
+				'긴 게시글 만들기긴 게시글 만들기긴 게시글 ssssssssssssssssssssssssssssssssssssssssssssssssssssssssss만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기 긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기',
+		},
+		{
+			id: 2,
+			title: '방탈출 후기입니다!',
+			author: '건주',
+			date: '2023-09-03',
+			content:
+				'긴 게시글 만들기긴 게시글 만들기긴 게시글 ssssssssssssssssssssssssssssssssssssssssssssssssssssssssss만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기 긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기',
+		},
+		{
+			id: 2,
+			title: '방탈출 후기입니다!',
+			author: '건주',
+			date: '2023-09-03',
+			content:
+				'긴 게시글 만들기긴 게시글 만들기긴 게시글 ssssssssssssssssssssssssssssssssssssssssssssssssssssssssss만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기 긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기',
+		},
+		{
+			id: 2,
+			title: '방탈출 후기입니다!',
+			author: '건주',
+			date: '2023-09-03',
+			content:
+				'긴 게시글 만들기긴 게시글 만들기긴 게시글 ssssssssssssssssssssssssssssssssssssssssssssssssssssssssss만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기 긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기',
+		},
+		{
+			id: 2,
+			title: '방탈출 후기입니다!',
+			author: '건주',
+			date: '2023-09-03',
+			content:
+				'긴 게시글 만들기긴 게시글 만들기긴 게시글 ssssssssssssssssssssssssssssssssssssssssssssssssssssssssss만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기 긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기',
+		},
+		{
+			id: 2,
+			title: '방탈출 후기입니다!',
+			author: '건주',
+			date: '2023-09-03',
+			content:
+				'긴 게시글 만들기긴 게시글 만들기긴 게시글 ssssssssssssssssssssssssssssssssssssssssssssssssssssssssss만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기 긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기',
+		},
+		{
+			id: 2,
+			title: '방탈출 후기입니다!',
+			author: '건주',
+			date: '2023-09-03',
+			content:
+				'긴 게시글 만들기긴 게시글 만들기긴 게시글 ssssssssssssssssssssssssssssssssssssssssssssssssssssssssss만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기 긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기긴 게시글 만들기',
+		},
 		// 추가적으로(?)
 	]);
 
@@ -46,8 +118,10 @@ function PostPage() {
 			<Helmet>
 				<title>게시글 목록</title>
 			</Helmet>
-			<div className="w-full max-w-[600px] min-w-[320px] bg-ec4  text-ec1 flex flex-col items-center h-screen m-auto gap-14">
-				<Header>게시글 목록</Header>
+			<div className="w-full max-w-[600px] min-w-[320px] py-20 bg-ec4 text-ec1 flex flex-col items-center h-full m-auto gap-14">
+				<div className="max-w-[600px] min-w-[320px] m-auto fixed top-0 left-0 right-0">
+					<Header>게시글 목록</Header>
+				</div>
 				<div className="text-[15px]">
 					<PostList posts={posts} />
 				</div>
