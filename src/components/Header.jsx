389aee41d506f2a1ffd2635cefd36d2a2af49884
skawls <ventu9204@gmail.com>
2023-09-08 14:59:29 +0900

    [수정] 헤더컴포넌트 코드수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,15 @@
 import { string } from 'prop-types';
 
 Header.propTypes = {
-  children: string,
+	children: string,
 };
 
 function Header({ children }) {
-  return (
-    <header className="py-3 text-center bg-ec4 text-ec1 text-2xl font-bold leading-2">
-      {children}
-    </header>
-  );
+	return (
+		<header className="py-3 text-center bg-ec4 text-ec1 text-2xl font-bold leading-2 flex justify-center items-center h-full">
+			{children}
+		</header>
+	);
 }
 
 export default Header;
