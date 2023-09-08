0eb1711e67288b99b69fbd29e844d134f63d115b
KIMGEUNDU <vlkoklv@naver.com>
2023-09-09 02:44:23 +0900

    [추가] 필요 코드 추가

--- src/routes.jsx
+++ src/routes.jsx
@@ -13,6 +13,7 @@ import PostPage from '@/pages/PostPage';
 import SignUp from '@/pages/SignUp';
 import AddCommunity from '@/pages/AddCommunity';
 import CommentPage from '@/pages/CommentPage';
+import Theme from './pages/Theme';
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
@@ -21,10 +22,12 @@ const router = createBrowserRouter(
 			<Route path="/loginselete" element={<LoginSelete />} />
 			<Route path="/login" element={<Login />} />
 			<Route path="/signup" element={<SignUp />} />
+			{/* 로그인 인증해야만 들어갈 수 있는 페이지 */}
+			<Route path="/theme" element={<Theme />} />
 			<Route path="/mypage" element={<Mypage />} />
 			<Route path="/editpage" element={<Editpage />} />
 			<Route path="/PostPage" element={<PostPage />} />
-			<Route path="/addcommunity" element={<AddCommunity />} />
+			<Route path="/addcommunity" element={<AddCommunity />} /> 
 			<Route path="/post/:id" element={<CommentPage />} />
 		</Route>
 	)
