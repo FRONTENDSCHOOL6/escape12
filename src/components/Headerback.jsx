b0d8ec0a8dc9098e02cc934253ec7cf5faaff7e8
skawls <ventu9204@gmail.com>
2023-09-08 14:59:38 +0900

    [수정] 해더백 컴포넌트 수정

--- src/components/Headerback.jsx
+++ src/components/Headerback.jsx
@@ -1,26 +1,23 @@
-import { string } from 'prop-types';
-import { Link } from 'react-router-dom';
+import { string, func } from 'prop-types';
 
 Headerback.propTypes = {
-  path: string,
-  children: string,
+	children: string,
+	onClick: func,
 };
 
-function Headerback({ path = "", children }) {
-  return (
-    <div>
-      <header className="py-3 text-center bg-ec4 text-ec1 text-2xl sm:text-xl font-bold leading-2">
-        <div className="absolute left-4 top-7 mx-auto max-w-screen-md flex">
-          <Link to={path}>
-            <button className='transform -translate-y-1/2 bg-transparent border-none '>
-              &lt;
-            </button>
-          </Link>
-          <div className="mx-auto">{children}</div>
-        </div>
-      </header>
-    </div>
-  );
+function Headerback({ onClick = '', children }) {
+	return (
+		<div>
+			<header className="py-3 text-center bg-ec4 text-ec1 text-2xl font-bold leading-2">
+				<div className="max-w-screen-md mx-auto flex items-center">
+					<button className="ml-2" onClick={onClick}>
+						&lt;
+					</button>
+					<div className="mx-auto flex-grow">{children}</div>
+				</div>
+			</header>
+		</div>
+	);
 }
 
 export default Headerback;
