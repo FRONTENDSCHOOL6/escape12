4b78308812fd6f4db0e1116dd50efc607e7f4978
skawls <ventu9204@gmail.com>
2023-09-08 14:59:56 +0900

    [수정] 포스트 컴포넌트 수정

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,40 +1,37 @@
 // import { useState } from 'react';
-import PropTypes from 'prop-types';
+import { string } from 'prop-types';
 import { Link } from 'react-router-dom';
 
 Post.propTypes = {
-	title: PropTypes.string.isRequired,
-	author: PropTypes.string.isRequired,
-	date: PropTypes.string.isRequired,
-	id: PropTypes.string.isRequired,
+	title: string,
+	author: string,
+	date: string,
+	id: string,
+	content: string,
 	// onDelete: PropTypes.func.isRequired,
 };
 
-function Post({ id, title, author, date /*onDelete*/ }) {
+function Post({ id, title, author, content /*onDelete*/ }) {
+	const shortContent =
+		content.length > 85 ? `${content.substring(0, 85)}...` : content;
 	// const [isClicked, setIsClicked] = useState(false);
 	// const handleDelete = () => {
 	// 	onDelete();
 	// };
 
 	return (
-		<div className="post max-w-[600px] min-w-[320px] bg-ec1 text-ec4 p-4 mb-4 rounded border flex flex-col justify-between ">
-			<div className="flex justify-between items-center">
-				<Link to={`/post/${id}`} className="post-title">
-					{title}
-				</Link>
-				<button
-					// onClick={handleDelete}
-					className="px-2 py-1 bg-ec3 text-ec1 rounded hover:text-ec5"
-				>
-					삭제
-				</button>
+		<Link to={`/post/${id}`}>
+			<div className="break-all text-[15px] h-[130px] border p-4 mb-4 rounded-xl min-w-[400px] max-w-[400px] flex flex-col m-auto">
+				<div>
+					<div className="flex justify-between border-b-[1px] mb-4 pb-2">
+						<p className="text-[15px]">{title}</p>
+						<p className="break-all font-bold text-[15px] ">{author}</p>
+					</div>
+					<p className="break-all">{shortContent}</p>
+				</div>
+				{/* {isClicked && <p className="">.재밌어요!</p>} */}
 			</div>
-			{/* {isClicked && <p className="">.재밌어요!</p>} */}
-			<div className="flex justify-between items-center mt-2">
-				<p className="post-author text-ec4 mb-1 mt-2">{author}</p>
-				<p className="post-date text-ec4">{date}</p>
-			</div>
-		</div>
+		</Link>
 	);
 }
 
