8a2b98ff89de117a2c0c171586c0b483003b1657
skawls <ventu9204@gmail.com>
2023-09-08 10:14:14 +0900

    [수정] PostClick페이지 Commen페이지로 이름변경

--- src/routes.jsx
+++ src/routes.jsx
@@ -12,7 +12,7 @@ import Editpage from './pages/EditPage';
 import PostPage from './pages/PostPage';
 import SignUp from './pages/SignUp';
 import AddCommunity from '@/pages/AddCommunity';
-import PostClick from '@/pages/PostClick';
+import CommentPage from '@/pages/CommentPage';
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
@@ -25,8 +25,7 @@ const router = createBrowserRouter(
 			<Route path="/editpage" element={<Editpage />} />
 			<Route path="/PostPage" element={<PostPage />} />
 			<Route path="/addcommunity" element={<AddCommunity />} />
-			<Route path="/postpage" element={<PostPage />} />
-			<Route path="/post/:id" element={<PostClick />} />
+			<Route path="/post/:id" element={<CommentPage />} />
 		</Route>
 	)
 );
