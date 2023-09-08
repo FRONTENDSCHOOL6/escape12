011ac67fff09a703ba676d9d1a64dc7543364122
KIMGEUNDU <vlkoklv@naver.com>
2023-09-08 14:42:32 +0900

    [수정] src/loginsignup 폴더로 위치 수정

---
+++ src/components/loginsignup/FormInput.jsx
@@ -0,0 +1,32 @@
+import { string, func } from 'prop-types';
+
+FormInput.propTypes = {
+	type: string,
+	name: string,
+	children: string,
+	onChange: func,
+};
+
+function FormInput({ children, type, name, onChange = null, ...restProps }) {
+	return (
+		<>
+			<fieldset className="flex gap-5 justify-between text-ec1">
+				<label htmlFor={name} className="w-32 s:w-28">
+					{children}
+				</label>
+				<input
+					type={type}
+					className="w-[300px] s:w-[200px] bg-ec4 border-b-2 border-ec1 focus:outline-none webkit"
+					name={name}
+					id={name}
+					onChange={onChange}
+					required
+					autoComplete="off"
+					{...restProps}
+				/>
+			</fieldset>
+		</>
+	);
+}
+
+export default FormInput;
