c9a9f5929f55c06429ef5f71f5061d89cc252d4b
skawls <ventu9204@gmail.com>
2023-09-09 17:33:46 +0900

    [수정] 네비바 isActive 동작안함 수정

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,52 +1,61 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useMatch } from 'react-router-dom';
 
 function Nav() {
-  return (
-    <nav className="text-center bg-ec3 text-ec1 text-base font-bold">
-      <ul className="flex justify-around items-center py-5">
-        <li className="hover:text-ec5">
-          <NavLink
-            to="theme"
-            className={({ isActive }) => {
-              isActive ? 'text-ec1' : 'text-ec5';
-            }}
-          >
-            테마
-          </NavLink>
-        </li>
-        <li className="hover:text-ec5">
-          <NavLink
-            to="record"
-            className={({ isActive }) => {
-              isActive ? 'text-ec1' : 'text-ec5';
-            }}
-          >
-            기록
-          </NavLink>
-        </li>
-        <li className="hover:text-ec5">
-          <NavLink
-            to="/addcommunity"
-            className={({ isActive }) => {
-              isActive ? 'text-ec1' : 'text-ec5';
-            }}
-          >
-            커뮤니티
-          </NavLink>
-        </li>
-        <li className="hover:text-ec5">
-          <NavLink
-            to="/mypage"
-            className={({ isActive }) => {
-              isActive ? 'text-ec1' : 'text-ec5';
-            }}
-          >
-            마이
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  );
+	const isPostPageActive = useMatch('/postpage');
+	const isNotePageActive = useMatch('/notepage');
+	const isAddCommunityActive = useMatch('/addcommunity');
+	const isMyPageActive = useMatch('/mypage');
+
+	return (
+		<nav className="text-center bg-ec3 text-base font-bold">
+			<ul className="flex justify-around items-center py-5">
+				<li>
+					<NavLink
+						to="/postpage"
+						activeClassName="text-ec5"
+						className={`text-ec1 hover:text-ec5 ${
+							isPostPageActive ? 'text-ec5' : ''
+						}`}
+					>
+						테마
+					</NavLink>
+				</li>
+				<li>
+					<NavLink
+						to="/notepage"
+						activeClassName="text-ec5"
+						className={`text-ec1 hover:text-ec5 ${
+							isNotePageActive ? 'text-ec5' : ''
+						}`}
+					>
+						기록
+					</NavLink>
+				</li>
+				<li>
+					<NavLink
+						to="/addcommunity"
+						activeClassName="text-ec5"
+						className={`text-ec1 hover:text-ec5 ${
+							isAddCommunityActive ? 'text-ec5' : ''
+						}`}
+					>
+						커뮤니티
+					</NavLink>
+				</li>
+				<li>
+					<NavLink
+						to="/mypage"
+						activeClassName="text-ec5"
+						className={`text-ec1 hover:text-ec5 ${
+							isMyPageActive ? 'text-ec5' : ''
+						}`}
+					>
+						마이
+					</NavLink>
+				</li>
+			</ul>
+		</nav>
+	);
 }
 
 export default Nav;
