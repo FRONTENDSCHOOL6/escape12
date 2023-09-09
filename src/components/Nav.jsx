6391e2dbe0b230c968d6f4dc9d5a1d4757a41256
JOSuYeoM <syjo1227@naver.com>
2023-09-09 20:26:28 +0900

    [수정] 컴포넌트 일부 수정

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,45 +1,46 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useMatch } from 'react-router-dom';
 
 function Nav() {
+  const isPostPageActive = useMatch('/Home');
+  const isNotePageActive = useMatch('/notepage');
+  const isAddCommunityActive = useMatch('/addcommunity');
+  const isMyPageActive = useMatch('/mypage');
+
   return (
-    <nav className="text-center bg-ec3 text-ec1 text-base font-bold">
-      <ul className="flex justify-around items-center py-5">
-        <li className="hover:text-ec5">
+    <nav className="text-center bg-ec3 text-ec1 font-bold max-w-[600px] min-w-[320px] m-auto fixed bottom-0 left-0 right-0">
+      <ul className='flex justify-around items-center py-5'>
+        <li>
           <NavLink
-            to="theme"
-            className={({ isActive }) => {
-              isActive ? 'text-ec1' : 'text-ec5';
-            }}
+            to='/Home'
+            activeClassName='text-ec5'
+            className={`text-ec1 hover:text-ec5 ${isPostPageActive ? 'text-ec5' : ''}`}
           >
             테마
           </NavLink>
         </li>
-        <li className="hover:text-ec5">
+        <li>
           <NavLink
-            to="record"
-            className={({ isActive }) => {
-              isActive ? 'text-ec1' : 'text-ec5';
-            }}
+            to='/notepage'
+            activeClassName='text-ec5'
+            className={`text-ec1 hover:text-ec5 ${isNotePageActive ? 'text-ec5' : ''}`}
           >
             기록
           </NavLink>
         </li>
-        <li className="hover:text-ec5">
+        <li>
           <NavLink
-            to="/addcommunity"
-            className={({ isActive }) => {
-              isActive ? 'text-ec1' : 'text-ec5';
-            }}
+            to='/addcommunity'
+            activeClassName='text-ec5'
+            className={`text-ec1 hover:text-ec5 ${isAddCommunityActive ? 'text-ec5' : ''}`}
           >
             커뮤니티
           </NavLink>
         </li>
-        <li className="hover:text-ec5">
+        <li>
           <NavLink
-            to="/mypage"
-            className={({ isActive }) => {
-              isActive ? 'text-ec1' : 'text-ec5';
-            }}
+            to='/mypage'
+            activeClassName='text-ec5'
+            className={`text-ec1 hover:text-ec5 ${isMyPageActive ? 'text-ec5' : ''}`}
           >
             마이
           </NavLink>
