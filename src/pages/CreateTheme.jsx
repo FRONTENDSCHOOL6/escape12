735d8e44adf2187e0e18e985ccd5dd1bd0b7aa77
KIMGEUNDU <vlkoklv@naver.com>
2023-09-10 04:07:39 +0900

    [수정] 많은 데이터 한번에 올리는 코드로 수정

--- src/pages/CreateTheme.jsx
+++ src/pages/CreateTheme.jsx
@@ -1,49 +1,21 @@
 import Header from '@/components/Header';
+import Nav from '@/components/Nav';
 import PocketBase from 'pocketbase';
 import { Helmet } from 'react-helmet-async';
 
 function CreateTheme() {
 	const pb = new PocketBase('https://refresh.pockethost.io');
 
-	const handleCreateData = async (themeData) => {
+	const handleCreateData = async () => {
+		const data = [];
 		try {
-			await pb.collection('escapeList').create(themeData);
+			for (let i = 0; i < data.length; i++) {
+				await pb.collection('escapeList').create(data[i]);
+			}
 		} catch (err) {
 			console.log(`에러 내용: ${err}`);
 		}
 	};
-
-	const themeDataArray = [
-		{
-			region: '강남',
-			store: '넥스트에디션',
-			point: '강남3호',
-			theme: '크리쳐 - 신인류의 탄생',
-			field: 'SF',
-			grade: 7,
-			level: 6,
-			image:
-				'https://next-edition.s3.amazonaws.com/theme/title_image_url/%ED%81%AC%EB%A6%AC%EC%B3%90%20-%20%EC%8B%A0%EC%9D%B8%EB%A5%98%EC%9D%98%20%ED%83%84%EC%83%9D/theme__%E1%84%8F%E1%85%B3%E1%84%85%E1%85%B5%E1%84%8E%E1%85%A7_%E1%84%91%E1%85%A9%E1%84%89%E1%85%B3%E1%84%90%E1%85%A5_%ED%81%AC%EB%A6%AC%EC%B3%90%20-%20%EC%8B%A0%EC%9D%B8%EB%A5%98%EC%9D%98%20%ED%83%84%EC%83%9D.jpg',
-			link: 'https://www.nextedition.co.kr/shops/Nextedition%20Gangnam3',
-		},
-		{
-			region: '강남',
-			store: '넥스트에디션',
-			point: '강남3호',
-			theme: '크리쳐 - 신인류의 탄생',
-			field: 'SF',
-			grade: 7,
-			level: 6,
-			image:
-				'https://next-edition.s3.amazonaws.com/theme/title_image_url/%ED%81%AC%EB%A6%AC%EC%B3%90%20-%20%EC%8B%A0%EC%9D%B8%EB%A5%98%EC%9D%98%20%ED%83%84%EC%83%9D/theme__%E1%84%8F%E1%85%B3%E1%84%85%E1%85%B5%E1%84%8E%E1%85%A7_%E1%84%91%E1%85%A9%E1%84%89%E1%85%B3%E1%84%90%E1%85%A5_%ED%81%AC%EB%A6%AC%EC%B3%90%20-%20%EC%8B%A0%EC%9D%B8%EB%A5%98%EC%9D%98%20%ED%83%84%EC%83%9D.jpg',
-			link: 'https://www.nextedition.co.kr/shops/Nextedition%20Gangnam3',
-		},
-	];
-
-	// 각 객체를 따로 등록
-	themeDataArray.forEach((themeData) => {
-		handleCreateData(themeData);
-	});
 
 	return (
 		<>
@@ -52,7 +24,6 @@ function CreateTheme() {
 			</Helmet>
 			<div className="max-w-[600px] min-w-[320px] bg-ec4 flex flex-col items-center h-screen m-auto py-20 relative">
 				<Header>테마 등록</Header>
-				<p className="">등록될까</p>
 				<button
 					onClick={handleCreateData}
 					type="button"
@@ -60,6 +31,7 @@ function CreateTheme() {
 				>
 					등록
 				</button>
+				<Nav />
 			</div>
 		</>
 	);
