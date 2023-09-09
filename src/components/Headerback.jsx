de449697fd4dbb6ac4f897acbb6642beb1493766
KIMGEUNDU <vlkoklv@naver.com>
2023-09-10 04:04:27 +0900

    [수정] 클린코드로 수정

--- src/components/Headerback.jsx
+++ src/components/Headerback.jsx
@@ -1,23 +1,19 @@
 import { string, func } from 'prop-types';
 
 Headerback.propTypes = {
-    children: string,
-    onClick: func,
+	children: string,
+	onClick: func,
 };
 
 function Headerback({ onClick = '', children }) {
-    return (
-        <div>
-            <header className="py-3 text-center bg-ec4 text-ec1 text-2xl font-bold leading-2">
-                <div className="max-w-screen-md mx-auto flex items-center">
-                    <button className="ml-2" onClick={onClick}>
-                        &lt;
-                    </button>
-                    <div className="mx-auto flex-grow">{children}</div>
-                </div>
-            </header>
-        </div>
-    );
+	return (
+		<header className="flex max-w-[600px] min-w-[320px] m-auto fixed top-0 left-0 right-0 py-3 text-center bg-ec4 text-ec1 text-2xl font-bold z-10">
+			<button className="absolute left-4" onClick={onClick}>
+				&lt;
+			</button>
+			<p className="mx-auto flex-grow">{children}</p>
+		</header>
+	);
 }
 
 export default Headerback;
