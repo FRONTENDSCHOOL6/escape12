18521faeb3a9782c326f040843ec5af8b0271a97
KIMGEUNDU <vlkoklv@naver.com>
2023-09-10 05:02:20 +0900

    [수정] 데이터 필드별로 props 수정

--- src/components/theme/ThemeItem.jsx
+++ src/components/theme/ThemeItem.jsx
@@ -1,8 +1,19 @@
+import { string, number } from 'prop-types';
 import { useState } from 'react';
 import HeartButton from './Heart';
 import Span from './Span';
 
-function ThemeItem() {
+ThemeItem.propTypes = {
+	store: string,
+	point: string,
+	theme: string,
+	grade: number,
+	level: number,
+	image: string,
+	link: string,
+};
+
+function ThemeItem({ store, point, theme, grade, level, image, link }) {
 	const [heart, setHeart] = useState(false);
 
 	const isClicked = () => {
@@ -13,28 +24,25 @@ function ThemeItem() {
 		<div className="w-full px-20 s:px-12 my-4">
 			<figure className="border-2 border-ec1 p-6 s:p-3 rounded-xl flex gap-3 text-ec1 text-lg s:text-base relative">
 				<div className="w-1/3 bg-ec4 flex">
-					<img
-						src="https://mblogthumb-phinf.pstatic.net/MjAyMTAzMTRfMjkw/MDAxNjE1NjQ4NDczMzc5.W5ncEzzylu4jd4vKZ98Ub0WmdnOBoiB0d_FMcKkBoIQg.iCaugZtjaQ4lbLBEGwXt2VoQBT9kkeLz8h6qdf8N0u8g.JPEG.rabbit4411/MIT12X6jhQHiFEousyiBFHCWOBe7huNeHMhH6cdU.jpg?type=w800"
-						alt="DONE"
-						aria-hidden
-						className="max-h-fit"
-					/>
+					<img src={image} alt={theme} aria-hidden className="max-h-fit" />
 				</div>
 				<figcaption className="flex flex-col justify">
 					<section className="flex items-end gap-5 s:gap-2">
-						<h2 className="text-4xl font-semibold s:text-2xl hover:text-ec5">
-							DONE
+						<h2 className="s:max-w-[90px] max-w-[170px] whitespace-nowrap overflow-hidden text-ellipsis text-2xl font-semibold s:text-2xl hover:text-ec5">
+							{theme}
 						</h2>
-						<Span>별점</Span>
+						<Span>{grade}</Span>
 					</section>
 					<section className="flex py-10 gap-3">
-						<h3>제로월드</h3>
-						<Span>강남점</Span>
+						<h3>{store}</h3>
+						<Span>{point}</Span>
 					</section>
 					<section className="flex gap-7 s:gap-2">
-						<Span>난이도</Span>
+						<Span>{level}</Span>
 						<Span>기록하기</Span>
-						<Span>예약하기</Span>
+						<a href={link}>
+							<Span>예약하기</Span>
+						</a>
 					</section>
 				</figcaption>
 				<HeartButton
