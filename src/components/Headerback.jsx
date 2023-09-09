8c84e8f3c9610a51a903b96654df4f90e016a374
JOSuYeoM <syjo1227@naver.com>
2023-09-09 22:48:15 +0900

    [수정] 컨포넌트 일부 수정

--- src/components/Headerback.jsx
+++ src/components/Headerback.jsx
@@ -7,16 +7,14 @@ Headerback.propTypes = {
 
 function Headerback({ onClick = '', children }) {
     return (
-        <div>
-            <header className="py-3 text-center bg-ec4 text-ec1 text-2xl font-bold leading-2">
-                <div className="max-w-screen-md mx-auto flex items-center">
-                    <button className="ml-2" onClick={onClick}>
-                        &lt;
-                    </button>
-                    <div className="mx-auto flex-grow">{children}</div>
-                </div>
-            </header>
-        </div>
+        <header className="max-w-[600px] min-w-[320px] m-auto fixed top-0 left-0 right-0 py-3 text-center bg-ec4 text-ec1 text-2xl font-bold z-10">
+            <div className="max-w-screen-md mx-auto flex items-center">
+                <button className="ml-2" onClick={onClick}>
+                    &lt;
+                </button>
+                <div className="mx-auto flex-grow">{children}</div>
+            </div>
+        </header>
     );
 }
 
