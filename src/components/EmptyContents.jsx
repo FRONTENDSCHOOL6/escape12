82475661d4013d64ea8b29d1f64f32b91f3aef6c
KIMGEUNDU <vlkoklv@naver.com>
2023-09-10 21:15:43 +0900

    [수정] logo이미지 png로 변경 후 관련 코드 수정

--- src/components/EmptyContents.jsx
+++ src/components/EmptyContents.jsx
@@ -4,7 +4,7 @@ function EmptyContents() {
 	return (
 		<>
 			<NotCogi />
-			<p className="text-center text-[30px] text-ec1 text-xl">
+			<p className="text-center text-[30px] text-ec1 text-xl mt-6">
 				게시글이 없습니다 :&#40;
 			</p>
 		</>
