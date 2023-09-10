4ff9979a9b22999c1a0361ce6aaaa59bdf8644ae
skawls <ventu9204@gmail.com>
2023-09-10 16:06:44 +0900

    [생성] 스몰버튼 생성(삭제,수정,등록)

---
+++ src/components/SmallButton.jsx
@@ -0,0 +1,19 @@
+import { string } from 'prop-types';
+
+SmallButton.propTypes = {
+	children: string,
+	bg: string,
+	text: string,
+};
+function SmallButton({ children, bg = 'bg-ec1', text = 'text-ec4' }) {
+	return (
+		<button
+			type="button"
+			className={`${text} ${bg} min-w-fit rounded-lg px-2 font-semibold leading-7 w-[30px]`}
+		>
+			{children}
+		</button>
+	);
+}
+
+export default SmallButton;
