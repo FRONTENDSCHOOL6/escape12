9d3a6f39aae9d113984bd047f7a42702b0a6b20d
skawls <ventu9204@gmail.com>
2023-09-10 12:55:47 +0900

    [수정] 검색인풋창 컴포넌트 수정

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,14 +1,21 @@
-function SearchInput() {
+import { string } from 'prop-types';
+
+SearchInput.propTypes = {
+	placeholder: string,
+	children: string,
+};
+
+function SearchInput({ placeholder, children }) {
 	return (
 		<div className="flex gap-4 w-full px-20 justify-center text-lg pb-4 s:px-12">
 			<input
 				type="text"
-				placeholder="방탈출"
+				placeholder={placeholder}
 				maxLength={35}
 				className="pl-3 py-1 rounded-full focus:outline-none flex-1"
 			/>
 			<button type="button" className="text-ec1 min-w-fit">
-				검색
+				{children}
 			</button>
 		</div>
 	);
