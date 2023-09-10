7c3b45f813664494fdd669dd56cb4a9bf88ddbb8
KIMGEUNDU <vlkoklv@naver.com>
2023-09-11 04:56:59 +0900

    [수정] 데이터불러오는 개수 수정

--- src/pages/Theme.jsx
+++ src/pages/Theme.jsx
@@ -190,7 +190,7 @@ function Theme() {
 		}
 
 		const escapeSearch = async () => {
-			const resultList = await pb.collection('escapeList').getList(1, 10, {
+			const resultList = await pb.collection('escapeList').getList(1, 200, {
 				filter: `(store ~ "${e.target.value}" || theme ~ "${e.target.value}")`,
 			});
 
