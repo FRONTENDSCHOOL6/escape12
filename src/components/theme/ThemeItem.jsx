a744d9a0652da3e8bc7200b857f786f4742caaa4
KIMGEUNDU <vlkoklv@naver.com>
2023-09-10 21:18:08 +0900

    [수정] 스타일 수정

--- src/components/theme/ThemeItem.jsx
+++ src/components/theme/ThemeItem.jsx
@@ -2,6 +2,7 @@ import { string, number } from 'prop-types';
 import { useState } from 'react';
 import HeartButton from './Heart';
 import Span from './Span';
+import { Link } from 'react-router-dom';
 
 ThemeItem.propTypes = {
 	store: string,
@@ -21,36 +22,34 @@ function ThemeItem({ store, point, theme, grade, level, image, link }) {
 	};
 
 	return (
-		<div className="w-full px-20 s:px-12 my-4">
-			<figure className="border-2 border-ec1 p-6 s:p-3 rounded-xl flex gap-3 text-ec1 text-lg s:text-base relative">
-				<div className="w-1/3 bg-ec4 flex">
-					<img src={image} alt={theme} aria-hidden className="max-h-fit" />
-				</div>
-				<figcaption className="flex flex-col justify">
-					<section className="flex items-end gap-5 s:gap-2">
-						<h2 className="s:max-w-[90px] max-w-[170px] whitespace-nowrap overflow-hidden text-ellipsis text-2xl font-semibold s:text-2xl hover:text-ec5">
-							{theme}
-						</h2>
-						<Span>{grade}</Span>
-					</section>
-					<section className="flex py-10 gap-3">
-						<h3>{store}</h3>
-						<Span>{point}</Span>
-					</section>
-					<section className="flex gap-7 s:gap-2">
-						<Span>{level}</Span>
-						<Span>기록하기</Span>
-						<a href={link}>
-							<Span>예약하기</Span>
-						</a>
-					</section>
-				</figcaption>
-				<HeartButton
-					onClick={isClicked}
-					checked={!heart ? 'bg-heartfalse' : 'bg-hearttrue'}
-				/>
-			</figure>
-		</div>
+		<figure className="my-4 border-2 border-ec1 p-6 s:p-3 rounded-xl flex gap-3 text-ec1 text-lg s:text-base relative max-h-[180px]">
+			<div className=" bg-ec4 flex w-[25%]">
+				<img src={image} alt={theme} aria-hidden className="w-full" />
+			</div>
+			<figcaption className="flex flex-col justify-between">
+				<section className="flex items-end gap-2 s:gap-2">
+					<h2 className="s:max-w-[90px] max-w-[170px] whitespace-nowrap overflow-hidden text-ellipsis text-2xl s:text-lg font-semibold">
+						{theme}
+					</h2>
+					<Span>평점 ⭐{grade}</Span>
+				</section>
+				<section className="flex gap-3">
+					<h3>{store}</h3>
+					<Span>{point}</Span>
+				</section>
+				<section className="flex gap-7 s:gap-2">
+					<Span>난이도 {level}</Span>
+					<Span>기록하기</Span>
+					<Link to={link} target="_blank">
+						<Span>예약하기</Span>
+					</Link>
+				</section>
+			</figcaption>
+			<HeartButton
+				onClick={isClicked}
+				checked={!heart ? 'bg-heartfalse' : 'bg-hearttrue'}
+			/>
+		</figure>
 	);
 }
 
