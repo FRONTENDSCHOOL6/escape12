3a9379c63584a3567be7cf97296c2806608d71e4
KIMGEUNDU <vlkoklv@naver.com>
2023-09-10 23:42:41 +0900

    [수정] onChange, value prop 수정

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,18 +1,22 @@
-import { string } from 'prop-types';
+import { string, func } from 'prop-types';
 import SmallButton from './SmallButton';
 
 SearchInput.propTypes = {
 	placeholder: string,
 	children: string,
+	value: string,
+	onChange: func,
 };
 
-function SearchInput({ placeholder, children }) {
+function SearchInput({ placeholder, children, value = '', onChange }) {
 	return (
 		<div className="flex gap-4 w-full px-20 justify-center text-lg pb-4 s:px-12">
 			<input
 				type="text"
 				placeholder={placeholder}
+				onChange={onChange}
 				maxLength={35}
+				value={value}
 				className="pl-3 py-1 rounded-full focus:outline-none flex-1"
 			/>
 			<SmallButton>{children}</SmallButton>
