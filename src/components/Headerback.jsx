2f4b1309b02f099349314206605b4d53a570ce8b
KIMGEUNDU <vlkoklv@naver.com>
2023-09-11 09:45:41 +0900

    [수정] Headerback 컴포넌트 수정

--- src/components/Headerback.jsx
+++ src/components/Headerback.jsx
@@ -1,18 +1,15 @@
-import { string } from 'prop-types';
+import { string, func } from 'prop-types';
 
 Headerback.propTypes = {
 	children: string,
+	onClick: func,
 };
 
-function Headerback({ children }) {
-	const handleGoBack = () => {
-		window.history.back();
-	};
-
+function Headerback({ onClick = '', children }) {
 	return (
 		<header className="max-w-[600px] min-w-[320px] m-auto fixed top-0 left-0 right-0 py-3 text-center bg-ec4 text-ec1 text-2xl font-bold z-10">
 			<div className="max-w-screen-md mx-auto flex items-center">
-				<button className="ml-2" onClick={handleGoBack}>
+				<button className="ml-2" onClick={onClick}>
 					&lt;
 				</button>
 				<div className="mx-auto flex-grow">{children}</div>
