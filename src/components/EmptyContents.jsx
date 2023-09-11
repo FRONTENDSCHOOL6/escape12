630e722007d908f14fe9e0be2d10b6e483a21681
JOSuYeoM <syjo1227@naver.com>
2023-09-11 10:06:29 +0900

    [해결] 이미지 포멧 및 성능 최적화 문제 해결

--- src/components/EmptyContents.jsx
+++ src/components/EmptyContents.jsx
@@ -1,11 +1,16 @@
 import NotCogi from './NotCogi';
+import { string } from 'prop-types';
 
-function EmptyContents() {
+EmptyContents.propTypes = {
+	children: string,
+};
+
+function EmptyContents({ children }) {
 	return (
 		<>
 			<NotCogi />
-			<p className="text-center text-[30px] text-ec1 text-xl">
-				게시글이 없습니다 :&#40;
+			<p className="text-center text-[30px] text-ec1 text-xl mt-6">
+				{children}
 			</p>
 		</>
 	);
