15b2efde826347e75c719c9b563427ca6b7246da
skawls <ventu9204@gmail.com>
2023-09-11 10:28:29 +0900

    [수정] Headerback 컴포넌트 수정

--- src/components/Headerback.jsx
+++ src/components/Headerback.jsx
@@ -7,11 +7,13 @@ Headerback.propTypes = {
 
 function Headerback({ onClick = '', children }) {
 	return (
-		<header className="flex max-w-[600px] min-w-[320px] m-auto fixed top-0 left-0 right-0 py-3 text-center bg-ec4 text-ec1 text-2xl font-bold z-10">
-			<button className="absolute left-4" onClick={onClick}>
-				&lt;
-			</button>
-			<p className="mx-auto flex-grow">{children}</p>
+		<header className="max-w-[600px] min-w-[320px] m-auto fixed top-0 left-0 right-0 py-3 text-center bg-ec4 text-ec1 text-2xl font-bold z-10">
+			<div className="max-w-screen-md mx-auto flex items-center">
+				<button className="ml-2" onClick={onClick}>
+					&lt;
+				</button>
+				<div className="mx-auto flex-grow">{children}</div>
+			</div>
 		</header>
 	);
 }
