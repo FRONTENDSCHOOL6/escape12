9ef30fafadb83f182f7f943248c08590d9cfab60
skawls <ventu9204@gmail.com>
2023-09-11 18:23:51 +0900

    [수정] 댓글 페이지 글자색 수정

--- src/components/comment/CommentPost.jsx
+++ src/components/comment/CommentPost.jsx
@@ -12,7 +12,7 @@ CommentPost.propTypes = {
 function CommentPost({ id, title, author, content }) {
 	return (
 		<Link to={`/post/${id}`}>
-			<div className="text-[15px] min-h-[400px] border p-4 mb-4 rounded-xl flex flex-col m-auto">
+			<div className="text-[15px] min-h-[400px] text-ec1 border p-4 mb-4 rounded-xl flex flex-col m-auto">
 				<div className="flex justify-between border-b-[1px] mb-4 pb-2">
 					<p>{title}제목</p>
 					<p className="font-bold">{author}작성자</p>
