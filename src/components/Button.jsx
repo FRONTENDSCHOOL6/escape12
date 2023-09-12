eeda9c996791e976e8cf651fef10b1668b6c9b0d
skawls <ventu9204@gmail.com>
2023-09-12 17:24:03 +0900

    [수정] 노트페이지 수정

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,29 +1,29 @@
 import { string, func } from 'prop-types';
 
 Button.propTypes = {
-    type: string,
-    bg: string,
-    text: string,
-    children: string,
-    onClick: func,
+	type: string,
+	bg: string,
+	text: string,
+	children: string,
+	onClick: func,
 };
 
 function Button({
-    type = 'button',
-    bg = 'bg-ec3',
-    text = 'bg-ec1',
-    onClick = '',
-    children,
+	type = 'button',
+	bg = 'bg-ec3',
+	text = 'bg-ec1',
+	onClick = 'null',
+	children,
 }) {
-    return (
-        <button
-            type={type}
-            className={`text-center rounded-lg ${bg} ${text} w-32 h-8 font-semibold`}
-            onClick={onClick}
-        >
-            {children}
-        </button>
-    );
+	return (
+		<button
+			type={type}
+			className={`text-center rounded-lg ${bg} ${text} w-32 h-8 font-semibold`}
+			onClick={onClick}
+		>
+			{children}
+		</button>
+	);
 }
 
 export default Button;
