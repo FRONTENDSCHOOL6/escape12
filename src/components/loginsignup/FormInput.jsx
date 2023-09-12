9ea2005634bfea74817f1f85bc7eba11707133e0
KIMGEUNDU <vlkoklv@naver.com>
2023-09-12 13:59:02 +0900

    [수정] FormInput 컴포넌트 타입 수정

--- src/components/loginsignup/FormInput.jsx
+++ src/components/loginsignup/FormInput.jsx
@@ -1,48 +1,51 @@
-import { string, func } from 'prop-types';
+import { func, string, object, number } from 'prop-types';
 
 FormInput.propTypes = {
-    type: string,
-    name: string,
-    bg: string,
-    children: string,
-    onChange: func,
-    onClick: func,
+	type: string,
+	name: string,
+	bg: string,
+	value: string || number,
+	children: object,
+	onChange: func,
+	onClick: func,
 };
 
 function FormInput({
-    children,
-    type,
-    name,
-    bg = 'hidden',
-    onChange = null,
-    onClick = null,
-    ...restProps
+	children,
+	type = 'text',
+	name,
+	bg = 'hidden',
+	value = '',
+	onChange = () => {},
+	onClick = () => {},
+	...restProps
 }) {
-    return (
-        <>
-            <fieldset className="flex gap-5 justify-between text-ec1 relative px-2">
-                <label htmlFor={name} className="w-32 s:min-w-fit">
-                    {children}
-                </label>
-                <input
-                    type={type}
-                    className="w-[300px] s:w-[90%] bg-opacity border-b-2 border-ec1 focus:outline-none"
-                    name={name}
-                    id={name}
-                    onChange={onChange}
-                    required
-                    maxLength={30}
-                    autoComplete="off"
-                    {...restProps}
-                />
-                <button
-                    type="button"
-                    className={`${bg} bg-cover w-5 h-5 absolute right-0 top-0`}
-                    onClick={onClick}
-                />
-            </fieldset>
-        </>
-    );
+	return (
+		<>
+			<div className="flex gap-5 justify-between text-ec1 relative px-2">
+				<label htmlFor={name} className="w-32 s:min-w-fit">
+					{children}
+				</label>
+				<input
+					type={type}
+					className="w-[300px] s:w-[90%] bg-opacity border-b-2 border-ec1 focus:outline-none"
+					name={name}
+					id={name}
+					onChange={onChange}
+					required
+					maxLength={30}
+					autoComplete="off"
+					value={value}
+					{...restProps}
+				/>
+				<button
+					type="button"
+					className={`${bg} bg-cover w-5 h-5 absolute right-0 top-0`}
+					onClick={onClick}
+				/>
+			</div>
+		</>
+	);
 }
 
 export default FormInput;
