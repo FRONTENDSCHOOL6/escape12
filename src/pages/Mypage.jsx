eae6e349a89c9b313a68697b8d4e8d89e5295b90
JOSuYeoM <syjo1227@naver.com>
2023-09-13 00:59:37 +0900

    [추가] 포켓베이스 불러오기 기능 추가

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -3,14 +3,46 @@ import Button from '@/components/Button';
 import Nav from '@/components/Nav';
 import Header from '@/components/Header';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import pb from '@/api/pockethost';
 
 function Mypage() {
   const navigate = useNavigate();
+  const [data, setData] = useState('')
+  const [records, setRecords] =useState([])
   //user 정보가 들어왔을 때 로그아웃 기능 구현
   const handleLogout = () => {
     localStorage.removeItem('user');
     navigate('/editpage');
   };
+
+  useEffect(() => {
+    const getrecord = async()=>{
+      const recordlist = await pb.collection('record').getFullList()
+    try {  
+      setRecords(recordlist)
+    } catch (error) {
+      console.log(error)
+    }
+    }
+    const datalist = async()=>{
+      const resultList = await pb.collection('users').getOne('b73e9sjwiu6hglx',{
+        expand:'email',
+    });
+
+  try { 
+    setData(resultList)
+  } catch (error) {
+    console.log(error)
+  }
+
+  
+}
+  getrecord(),
+  datalist()
+  },[])
+  
 
   return (
     <>
@@ -20,23 +52,24 @@ function Mypage() {
       <div className="max-w-[600px] min-w-[320px] bg-ec4 text-ec1 flex flex-col items-center min-h-[100vh] m-auto py-20 relative">
         {/* header, headerback 맨 위 고정 */}
         <Header>마이페이지</Header>
-        <div className='flex-1 flex flex-col items-center'>
-          <div className='text-xl pt-20'>
-            <p className='pb-2'>아이디| 12조 최고 ~ </p>
-            <p className='pb-2'>비밀번호| ********</p>
-            <p className='pb-2'>닉네임| </p>
-          </div>
+
+        <div className="flex-1 flex flex-col items-center">
+          <ul className="text-xl pt-20 space-y-4">
+            <li>아이디 | {data.email} </li>
+            <li>비밀번호 | ******** </li>
+            <li>닉네임 | {data.nickName} </li>
+          </ul>
           <Button
             onClick={() => { navigate('/editpage'); }} bg="bg-ec1"
             text="text-ec4 mt-8"
           >정보수정
           </Button>
-          <div className="border-2 p-8 mt-32 text-xl rounded-xl text-center">
-            <p className>내가 작성한 기록: n개 </p>
-            <p className>내가 작성한 글: n개</p>
-            <p className>내가 작성한 댓글: n개 </p>
-            <p className>⭐ 즐겨찾기 </p>
-          </div>
+          <ul className="border-2 p-12 space-y-4 mt-16 text-xl rounded-xl text-center">
+            <li className>내가 작성한 기록: {records.length} 개</li>
+            <li className>내가 작성한 글: {records.length} 개</li>
+            <li className>내가 작성한 댓글: {records.length} 개</li>
+            <li className>⭐ 즐겨찾기 </li>
+          </ul>
           <Button
             onClick={handleLogout}
             bg="bg-ec1 text-center mt-8"
