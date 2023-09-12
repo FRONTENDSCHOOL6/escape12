9e075f0cee7678b0a746f56997711f270ef1993a
skawls <ventu9204@gmail.com>
2023-09-12 17:23:16 +0900

    [수정] Theme페이지 수정

--- src/pages/ThemeRecord.jsx
+++ src/pages/ThemeRecord.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import pb from '@/api/pockethost';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 function ThemeRecord() {
-  return (
-    <div>ThemeRecord</div>
-  )
+	const { dataId } = useParams();
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		console.log(dataId);
+		const dataList = async () => {
+			const record = await pb.collection('escapeList').getOne(`${dataId}`);
+			try {
+				setData(record);
+				console.log(record);
+			} catch (err) {
+				console.log(`에러 내용: ${err}`);
+			}
+		};
+		dataList();
+	}, [dataId]);
+
+	return (
+		<div>
+			<p>{data.theme}</p>
+			<p>{data.store}</p>
+			<p>{data.grade}</p>
+			<img src={data.image} alt={data.theme} />
+		</div>
+	);
 }
 
-export default ThemeRecord
+export default ThemeRecord;
