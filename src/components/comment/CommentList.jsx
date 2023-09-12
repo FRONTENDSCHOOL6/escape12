b517aff1876be83db02390bdbf7831f3be2131f3
skawls <ventu9204@gmail.com>
2023-09-12 17:27:09 +0900

    [수정] 댓글페이지 리스트 타입수정 및 댓글페이지 포켓호스트 수정

--- src/components/comment/CommentList.jsx
+++ src/components/comment/CommentList.jsx
@@ -1,28 +1,29 @@
-import { arrayOf, shape, number, string } from 'prop-types';
+import { arrayOf, shape, string } from 'prop-types';
 import Comment from '@/components/comment/Comment';
 
 CommentList.propTypes = {
-    comments: arrayOf(
-        shape({
-            id: number,
-            author: string,
-            content: string,
-        })
-    ).isRequired,
+	comments: arrayOf(
+		shape({
+			id: string,
+			author: string,
+			content: string,
+		})
+	).isRequired,
 };
 
 function CommentList({ comments }) {
-    return (
-        <div className="w-full s:px-12 px-20">
-            {comments.map((comment) => (
-                <Comment
-                    key={comment.id}
-                    author={comment.author}
-                    content={comment.content}
-                />
-            ))}
-        </div>
-    );
+	console.log(comments);
+	return (
+		<div className="w-full s:px-12 px-20">
+			{comments.map((comment) => (
+				<Comment
+					key={comment.id}
+					author={comment.author}
+					content={comment.content}
+				/>
+			))}
+		</div>
+	);
 }
 
 export default CommentList;
