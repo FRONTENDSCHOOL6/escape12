8fd98e7f2b5be96be67a04eb29e6eed43c581994
KIMGEUNDU <vlkoklv@naver.com>
2023-09-13 05:57:14 +0900

    [추가] 길이 fit 고정

--- src/components/SmallButton.jsx
+++ src/components/SmallButton.jsx
@@ -9,7 +9,7 @@ function SmallButton({ children, bg = 'bg-ec1', text = 'text-ec4' }) {
 	return (
 		<button
 			type="button"
-			className={`${text} ${bg} min-w-fit rounded-lg px-2 font-semibold leading-7`}
+			className={`${text} ${bg} min-w-fit rounded-lg px-2 font-semibold leading-7 whitespace-nowrap s:px-1`}
 		>
 			{children}
 		</button>
