c461fa43cbe3948d3960dfa5c3b850f3e4885f28
JOSuYeoM <syjo1227@naver.com>
2023-09-13 13:34:29 +0900

    [수정] 절대경로 수정 및 파일명 변경

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,19 +1,19 @@
 import { createBrowserRouter } from 'react-router-dom';
-import Home from './pages/Home';
-import LoginSelete from './pages/LoginSelete';
-import Empty from './layout/Empty';
-import Login from './pages/Login';
-import SignUp from './pages/SignUp';
-import Theme from './pages/Theme';
-import Escape from './layout/Escape';
-import ThemeRecord from './pages/ThemeRecord';
-import Mypage from './pages/Mypage';
-import Editpage from './pages/Editpage';
-import PostPage from './pages/PostPage';
-import NotePage from './pages/NotePage';
-import CommentPage from './pages/CommentPage';
-import ProtectRoute from './components/ProtectRoute';
-import AddCommunity from './pages/AddCommunity';
+import Home from '@/pages/Home';
+import LoginSelete from '@/pages/LoginSelete';
+import Empty from '@/layout/Empty';
+import Login from '@/pages/Login';
+import SignUp from '@/pages/SignUp';
+import Theme from '@/pages/Theme';
+import Escape from '@/layout/Escape';
+import ThemeRecord from '@/pages/ThemeRecord';
+import Mypage from '@/pages/Mypage';
+import Editpage from '@/pages/Editpage';
+import PostPage from '@/pages/PostPage';
+import RecordPage from '@/pages/RecordPage';
+import CommentPage from '@/pages/CommentPage';
+import ProtectRoute from '@/components/ProtectRoute';
+import AddCommunity from '@/pages/AddCommunity';
 
 const router = createBrowserRouter([
     {
@@ -41,7 +41,7 @@ const router = createBrowserRouter([
             { path: '/addcommunity', element: <AddCommunity /> },
             { path: '/editpage', element: <Editpage /> },
             { path: '/post/:dataId', element: <CommentPage /> },
-            { path: '/notepage', element: <NotePage /> },
+            { path: '/recordpage', element: <RecordPage/> },
         ],
     },
 ]);
