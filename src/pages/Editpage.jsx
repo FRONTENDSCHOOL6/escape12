01146c5021eb07e49c77f548baed3e5d7351caa4
JOSuYeoM <syjo1227@naver.com>
2023-09-13 13:36:17 +0900

    [수정] UI 수정 완료

--- src/pages/Editpage.jsx
+++ src/pages/Editpage.jsx
@@ -3,6 +3,7 @@ import Button from '@/components/Button';
 import Nav from '@/components/Nav';
 import Headerback from '@/components/HeaderBack';
 import { useNavigate } from 'react-router-dom';
+import FormInput from '@/components/loginsignup/FormInput';
 
 function Editpage() {
   const navigate = useNavigate();
@@ -14,18 +15,47 @@ function Editpage() {
       </Helmet>
       <div className="max-w-[600px] min-w-[320px] bg-ec4 text-ec1 flex flex-col items-center min-h-[100vh] m-auto py-20 relative">
         {/* header, headerback 맨 위 고정 */}
-          <Headerback onClick={() => { 
-            navigate('/mypage'); }}
-            >정보 수정</Headerback>
-        <div className='flex-1 flex flex-col items-center'>
-          <div className='text-2xl pt-40'>
-            <p className='pb-2'>아이디| 12조 최고 ~ </p>
-            <p className='pb-2'>비밀번호| ********</p>
-            <p className='pb-2'>닉네임| </p>
+        <Headerback onClick={() => {
+          navigate('/mypage');
+        }}
+        >정보 수정</Headerback>
+        <div className="flex-1 flex flex-col items-center">
+          <div className="s:px-12 p-12 text-xl space-y-10">
+          <img
+						className="w-[30%] mx-auto rounded-full"
+						src="https://mblogthumb-phinf.pstatic.net/MjAxOTAxMjNfMjI5/MDAxNTQ4MTcxMTE2MTI4.nv3-mRR-cZiGBxCD_KuMH8OsQ-WDJEJ9kTTBwb2XlkUg.WKv1PpzrR2s0duklK1AemD8cmGDAvRre7yrJG1okdZ8g.JPEG.seooooya/IMG_2063.JPG?type=w800"
+						alt="사용자 사진"
+						aria-hidden
+					/>
+          <FormInput
+								type="email"
+								name="id"
+								placeholder="변경할 이메일"
+							>
+								아이디(이메일)
+							</FormInput>
+              <FormInput
+								type="password"
+								name="password"
+								placeholder="변경할 비밀번호"
+							>
+								비밀번호
+							</FormInput>
+              <FormInput
+								type="text"
+								name="nickname"
+								placeholder="닉네임"
+							>
+								닉네임
+							</FormInput>
           </div>
-            <Button 
-            onClick={() => { navigate('/mypage'); }} bg='bg-ec1' 
+          <Button
+            onClick={() => { navigate('/mypage'); }} bg='bg-ec1'
             text='text-ec4 mt-4'>저장</Button>
+          <footer className='mt-auto py-1'>
+            <em><a href="https://github.com/FRONTENDSCHOOL6/escape12">Copyright &copy; 2023 김건주, 김남진, 조수연
+            </a></em>
+          </footer>
         </div>
       </div>
       <Nav />
--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -2,7 +2,7 @@ import { Helmet } from 'react-helmet-async';
 import Button from '@/components/Button';
 import Nav from '@/components/Nav';
 import Header from '@/components/Header';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import pb from '@/api/pockethost';
@@ -10,7 +10,8 @@ import pb from '@/api/pockethost';
 function Mypage() {
   const navigate = useNavigate();
   const [data, setData] = useState('')
-  const [records, setRecords] =useState([])
+  const [records, setRecords] = useState([])
+
   //user 정보가 들어왔을 때 로그아웃 기능 구현
   const handleLogout = () => {
     localStorage.removeItem('user');
@@ -18,31 +19,31 @@ function Mypage() {
   };
 
   useEffect(() => {
-    const getrecord = async()=>{
+    const getrecord = async () => {
       const recordlist = await pb.collection('record').getFullList()
-    try {  
-      setRecords(recordlist)
-    } catch (error) {
-      console.log(error)
+      try {
+        setRecords(recordlist)
+      } catch (error) {
+        console.log(error)
+      }
     }
+    const datalist = async () => {
+      const resultList = await pb.collection('users').getOne('b73e9sjwiu6hglx', {
+        expand: 'email',
+      });
+
+      try {
+        setData(resultList)
+      } catch (error) {
+        console.log(error)
+      }
+
+
     }
-    const datalist = async()=>{
-      const resultList = await pb.collection('users').getOne('b73e9sjwiu6hglx',{
-        expand:'email',
-    });
+    getrecord(),
+      datalist()
+  }, [])
 
-  try { 
-    setData(resultList)
-  } catch (error) {
-    console.log(error)
-  }
-
-  
-}
-  getrecord(),
-  datalist()
-  },[])
-  
 
   return (
     <>
@@ -54,21 +55,38 @@ function Mypage() {
         <Header>마이페이지</Header>
 
         <div className="flex-1 flex flex-col items-center">
-          <ul className="text-xl pt-20 space-y-4">
+        <img
+						className="w-[30%] mx-auto rounded-full"
+						src="https://mblogthumb-phinf.pstatic.net/MjAxOTAxMjNfMjI5/MDAxNTQ4MTcxMTE2MTI4.nv3-mRR-cZiGBxCD_KuMH8OsQ-WDJEJ9kTTBwb2XlkUg.WKv1PpzrR2s0duklK1AemD8cmGDAvRre7yrJG1okdZ8g.JPEG.seooooya/IMG_2063.JPG?type=w800"
+						alt="사용자 사진"
+						aria-hidden
+					/>
+          <ul className="s:px-12 p-12 text-xl space-y-4">
             <li>아이디 | {data.email} </li>
             <li>비밀번호 | ******** </li>
             <li>닉네임 | {data.nickName} </li>
           </ul>
           <Button
-            onClick={() => { navigate('/editpage'); }} bg="bg-ec1"
-            text="text-ec4 mt-8"
+            onClick={() => { navigate('/editpage'); }}
+            bg="bg-ec1"
+            text="text-ec4"
           >정보수정
           </Button>
-          <ul className="border-2 p-12 space-y-4 mt-16 text-xl rounded-xl text-center">
-            <li className>내가 작성한 기록: {records.length} 개</li>
-            <li className>내가 작성한 글: {records.length} 개</li>
-            <li className>내가 작성한 댓글: {records.length} 개</li>
-            <li className>⭐ 즐겨찾기 </li>
+          <ul className="w-80 s:px-12 border-2 p-12 text-xl space-y-4 mt-8 text-center">
+            <li>
+              내가 작성한 기록 :
+              <Link to="/recordpage" className="hover:text-ec5"> {records.length} 개</Link>
+            </li>
+            <li>
+              내가 작성한 글 :
+              <Link to="/mypage" className="hover:text-ec5"> {records.length} 개</Link></li>
+            <li>
+              내가 작성한 댓글 :
+              <Link to="/mypage" className="hover:text-ec5"> {records.length} 개</Link></li>
+            <li
+            >
+              <Link to="/mypage" className="hover:text-ec5">
+                ⭐ 즐겨찾기 바로가기 </Link></li>
           </ul>
           <Button
             onClick={handleLogout}
