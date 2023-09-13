c9f9c61a09de54b32b1f56c91445c0d958863079
KIMGEUNDU <vlkoklv@naver.com>
2023-09-13 17:30:03 +0900

    [수정] width값 수정

--- src/components/SmallButton.jsx
+++ src/components/SmallButton.jsx
@@ -9,7 +9,7 @@ function SmallButton({ children, bg = 'bg-ec1', text = 'text-ec4' }) {
 	return (
 		<button
 			type="button"
-			className={`${text} ${bg} min-w-fit rounded-lg px-2 font-semibold leading-7 whitespace-nowrap s:px-1`}
+			className={`${text} ${bg} min-w-fit w-[20%] rounded-lg px-2 font-semibold leading-7 whitespace-nowrap s:px-1`}
 		>
 			{children}
 		</button>
