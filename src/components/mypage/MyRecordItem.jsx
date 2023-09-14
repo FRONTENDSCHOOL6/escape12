0ba4c736b4b43b3fdf7e7bd2d66ac91a71ddbd96
KIMGEUNDU <vlkoklv@naver.com>
2023-09-15 03:05:33 +0900

    [추가] 데이터불러오기, 검색기능 추가

--- src/components/mypage/MyRecordItem.jsx
+++ src/components/mypage/MyRecordItem.jsx
@@ -1,40 +1,52 @@
-import { Link } from 'react-router-dom';
-import Span from '../theme/Span';
+import { number, string } from 'prop-types';
+import { useNavigate } from 'react-router-dom';
 import Button from '../button/Button';
+import Span from '../theme/Span';
 
-function MyRecordItem() {
+MyRecordItem.propTypes = {
+	link: string,
+	src: string,
+	alt: string,
+	theme: string,
+	store: string,
+	grade: number,
+};
+
+function MyRecordItem({ link, src, alt, theme, store, grade }) {
+	const navigate = useNavigate();
+
+	const handleLink = () => {
+		navigate(`/upload/${link}`);
+	};
+
 	return (
-		<Link to="">
-			<figure className="my-1 border-2 border-ec1 p-6 s:p-3 rounded-xl flex gap-3 text-ec1 text-lg s:text-base relative h-[180px]">
-				<div className=" bg-ec4 flex w-[25%]">
-					<img
-						src="https://blog.kakaocdn.net/dn/N7Dgj/btrHVz75l25/offKewdp8ZCnb9uqM7SuV0/img.png"
-						alt="사그행부"
-						aria-hidden
-						className="w-full"
-					/>
-				</div>
-				<figcaption className="flex flex-col justify-between s:justify-around">
-					<section className="flex items-end justify-between">
-						<h2 className="s:max-w-[90px] max-w-[170px] whitespace-nowrap overflow-hidden text-ellipsis text-2xl s:text-lg font-semibold">
-							사람들은 그것을 행복이라고 부르기로 했다
-						</h2>
-						<Span>평점 ⭐9</Span>
-					</section>
-					<section className="flex justify-between">
-						<h3 className="whitespace-nowrap">
-							키이스케이프 <Span>홍대점</Span>
-						</h3>
-						<Span>난이도 8</Span>
-					</section>
-					<section className="mx-auto">
-						<Button bg="bg-ec1" text="text-ec4">
-							보러가기
-						</Button>
-					</section>
-				</figcaption>
-			</figure>
-		</Link>
+		<figure className="mb-5 border-2 border-ec1 p-4 s:p-3 rounded-xl flex gap-3 text-ec1 text-lg s:text-base relative h-[180px]">
+			<div className=" bg-ec4 flex w-[200px]">
+				<img src={src} alt={alt} aria-hidden className="w-full" />
+			</div>
+			<figcaption className="flex flex-col justify-between s:justify-around w-full">
+				<section className="flex justify-between">
+					<h2 className="s:max-w-[90px] max-w-[170px] whitespace-nowrap overflow-hidden text-ellipsis text-2xl s:text-lg font-semibold">
+						{theme}
+					</h2>
+					<Span>
+						{grade === 0 || grade === 1 || grade === 2 || grade === 3
+							? '😱흙길'
+							: grade === 4 || grade === 5 || grade === 6 || grade === 7
+							? '🌿풀길'
+							: '🌸꽃길'}
+					</Span>
+				</section>
+				<section className="flex justify-between">
+					<h3 className="whitespace-nowrap">{store}</h3>
+				</section>
+				<section className="text-right">
+					<Button bg="bg-ec1" text="text-ec4" onClick={handleLink}>
+						보러가기
+					</Button>
+				</section>
+			</figcaption>
+		</figure>
 	);
 }
 
