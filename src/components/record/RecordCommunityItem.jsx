a9750d2883e263b4408ec2d51c468565f92d69df
KIMGEUNDU <vlkoklv@naver.com>
2023-09-15 17:34:36 +0900

    [생성] RecordCommunityItem 생성

---
+++ src/components/record/RecordCommunityItem.jsx
@@ -0,0 +1,53 @@
+import { number, string } from 'prop-types';
+import Button from '../button/Button';
+import Span from '../theme/Span';
+import { useNavigate } from 'react-router-dom';
+
+RecordCommunityItem.propTypes = {
+	store: string,
+	theme: string,
+	grade: number,
+	image: string,
+	author: string,
+	link: string,
+};
+
+function RecordCommunityItem({ store, theme, grade, image, author, link }) {
+	const navigate = useNavigate();
+
+	const handleLink = () => {
+		navigate(`/upload/${link}`);
+	};
+	return (
+		<figure className="my-4 border-2 border-ec1 p-4 s:p-3 rounded-xl flex gap-3 s:gap-[5%] text-ec1 text-lg s:text-base relative h-[180px]">
+			<div className=" bg-ec4 flex w-[50%]">
+				<img src={image} alt={theme} aria-hidden className="w-full" />
+			</div>
+			<figcaption className="flex flex-col justify-between s:justify-around w-full">
+				<section className="flex w-full justify-between">
+					<h2 className="s:max-w-[90px] max-w-[170px] whitespace-nowrap overflow-hidden text-ellipsis text-2xl s:text-lg font-semibold">
+						{theme}
+					</h2>
+					<Span>
+						{grade === 0 || grade === 1 || grade === 2 || grade === 3
+							? '😱흙길'
+							: grade === 4 || grade === 5 || grade === 6 || grade === 7
+							? '🌿풀길'
+							: '🌸꽃길'}
+					</Span>
+				</section>
+				<section className="flex gap-3">
+					<h3>{store}</h3>
+				</section>
+				<section className="flex justify-between">
+					<span>{author}</span>
+					<Button bg="bg-ec1" text="text-ec4" onClick={handleLink}>
+						보러가기
+					</Button>
+				</section>
+			</figcaption>
+		</figure>
+	);
+}
+
+export default RecordCommunityItem;
