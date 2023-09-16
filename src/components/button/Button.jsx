fcd8b886a635f36fe52426cb0025ae3d06880ed0
JOSuYeoM <syjo1227@naver.com>
2023-09-16 17:36:30 +0900

    [수정] button 컴포넌트 길이 수정

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,29 +1,29 @@
 import { string, func } from 'prop-types';
 
 Button.propTypes = {
-	type: string,
-	bg: string,
-	text: string,
-	children: string,
-	onClick: func,
+    type: string,
+    bg: string,
+    text: string,
+    children: string,
+    onClick: func,
 };
 
 function Button({
-	type = 'button',
-	bg = 'bg-ec3',
-	text = 'bg-ec1',
-	onClick = null,
-	children,
+    type = 'button',
+    bg = 'bg-ec3',
+    text = 'bg-ec1',
+    onClick = null,
+    children,
 }) {
-	return (
-		<button
-			type={type}
-			className={`text-center rounded-lg ${bg} ${text} px-[10%] h-8 font-semibold`}
-			onClick={onClick}
-		>
-			{children}
-		</button>
-	);
+    return (
+        <button
+            type={type}
+            className={`text-center rounded-lg ${bg} ${text} px-8 h-8 font-semibold`}
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    );
 }
 
 export default Button;
