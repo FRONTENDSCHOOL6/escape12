544157addee728a9beb6c4ebb58602ed81f88956
JOSuYeoM <syjo1227@naver.com>
2023-09-16 20:21:33 +0900

    [병합] mypage, develop 병합 완료

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,19 +1,19 @@
 import { string, func } from 'prop-types';
 
 Button.propTypes = {
-    type: string,
-    bg: string,
-    text: string,
-    children: string,
-    onClick: func,
+	type: string,
+	bg: string,
+	text: string,
+	children: string,
+	onClick: func,
 };
 
 function Button({
-    type = 'button',
-    bg = 'bg-ec3',
-    text = 'bg-ec1',
-    onClick = null,
-    children,
+	type = 'button',
+	bg = 'bg-ec3',
+	text = 'bg-ec1',
+	onClick = null,
+	children,
 }) {
 	return (
 		<button
