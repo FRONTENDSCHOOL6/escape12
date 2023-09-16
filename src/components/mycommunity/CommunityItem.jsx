6fb1f20db4e5dbc41b9a5dd7fa0575ff195df649
skawls <ventu9204@gmail.com>
2023-09-16 20:07:55 +0900

    [수정] 내가쓴게시물 컴포넌트(삭제예정)

--- src/components/mycommunity/CommunityItem.jsx
+++ src/components/mycommunity/CommunityItem.jsx
@@ -1,30 +1,30 @@
-import { string } from 'prop-types';
-import { Link } from 'react-router-dom';
+// import { string } from 'prop-types';
+// import { Link } from 'react-router-dom';
 
-Community.propTypes = {
-	id: string,
-	title: string,
-	content: string,
-};
+// Community.propTypes = {
+// 	id: string,
+// 	title: string,
+// 	content: string,
+// };
 
-function Community({ id, title, content }) {
-	const shortContent =
-		content && content.length > 85 ? `${content.substring(0, 85)}...` : content;
+// function Community({ id, title, content }) {
+// 	const shortContent =
+// 		content && content.length > 85 ? `${content.substring(0, 85)}...` : content;
 
-	return (
-		<Link to={`/CommentPage/${id}`}>
-			<div className="text-ec1 border-2 max-h-[105px] p-4 mb-6 rounded-xl flex flex-col m-auto ">
-				<div className="flex justify-between border-b-[1px] mb-4 pb-2 ">
-					<p className="whitespace-nowrap overflow-hidden text-ellipsis">
-						{title}임시제목
-					</p>
-				</div>
-				<p className="break-all whitespace-nowrap overflow-hidden text-ellipsis">
-					{shortContent}임시 본문 ㅇㅇㅇㅇㅇㅇㅇ
-				</p>
-			</div>
-		</Link>
-	);
-}
+// 	return (
+// 		<Link to={`/postpage/${id}`}>
+// 			<div className="text-ec1 border-2 max-h-[105px] p-4 mb-6 rounded-xl flex flex-col m-auto ">
+// 				<div className="flex justify-between border-b-[1px] mb-4 pb-2 ">
+// 					<p className="whitespace-nowrap overflow-hidden text-ellipsis">
+// 						{title}
+// 					</p>
+// 				</div>
+// 				<p className="break-all whitespace-nowrap overflow-hidden text-ellipsis">
+// 					{shortContent}
+// 				</p>
+// 			</div>
+// 		</Link>
+// 	);
+// }
 
-export default Community;
+// export default Community;
