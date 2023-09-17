08f3114016ee8121661e8aabf61e78e81df336f3
skawls <ventu9204@gmail.com>
2023-09-17 18:23:51 +0900

    [수정] 호버 변경

--- src/components/post/PostItem.jsx
+++ src/components/post/PostItem.jsx
@@ -1,4 +1,3 @@
-// import { useState } from 'react';
 import { string } from 'prop-types';
 import { Link } from 'react-router-dom';
 
@@ -16,7 +15,7 @@ function PostItem({ id, title, author, content }) {
 
 	return (
 		<Link to={`/postpage/${id}`}>
-			<div className="text-ec1 border-2  max-h-[105px] p-4 mb-6 rounded-xl flex flex-col m-auto">
+			<div className="text-ec1 border-2 max-h-[105px] p-4 mb-6 rounded-xl flex flex-col m-auto transition-transform duration-500 ease-in-out transform hover:scale-105">
 				<div className="flex justify-between border-b-[1px] mb-4 pb-2">
 					<p className="whitespace-nowrap overflow-hidden text-ellipsis">
 						{title}
@@ -26,7 +25,6 @@ function PostItem({ id, title, author, content }) {
 				<p className="break-all whitespace-nowrap overflow-hidden text-ellipsis">
 					{shortContent}
 				</p>
-				{/* {isClicked && <p className="">.재밌어요!</p>} */}
 			</div>
 		</Link>
 	);
