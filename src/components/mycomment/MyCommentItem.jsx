deea008db8891cefc1d3c2778348eeed29761fcf
skawls <ventu9204@gmail.com>
2023-09-17 18:23:18 +0900

    [수정] mycomment에 삭제버튼 수정

--- src/components/mycomment/MyCommentItem.jsx
+++ src/components/mycomment/MyCommentItem.jsx
@@ -1,17 +1,44 @@
 import { string } from 'prop-types';
 import SmallButton from '../button/SmallButton';
+import toast from 'react-hot-toast';
+import pb from '@/api/pockethost';
+import { Link } from 'react-router-dom';
 
 MyCommentItem.propTypes = {
 	src: string,
 	alt: string,
 	nickName: string,
 	comment: string,
+	id: string,
+	postId: string,
+	recordId: string,
 };
 
-function MyCommentItem({ comment }) {
+function MyCommentItem({ comment, id, postId, recordId }) {
+	// console.log(postId);
+	const handleDeletecomment = async (e) => {
+		e.preventDefault();
+		const deleteConfirm = confirm('정말로 삭제하시겠습니까?');
+
+		try {
+			if (deleteConfirm) {
+				await pb.collection('comment').delete(`${id}`);
+
+				toast('삭제되었습니다', {
+					icon: '🗑️',
+					duration: 2000,
+				});
+
+				location.reload();
+			}
+		} catch (err) {
+			console.log(`삭제 에러: ${err}`);
+		}
+	};
+
 	return (
-		<>
-			<div className="text-ec1 border-2 justify-between max-h-[105px] p-4 mb-6 rounded-xl flex m-auto">
+		<div className="text-ec1 border-2 justify-between max-h-[105px] p-4 mb-6 rounded-xl flex m-auto transition-transform duration-500 ease-in-out transform hover:scale-105">
+			<Link to={`/postpage/${postId}`}>
 				<div className="">
 					{/* <img className="w-8 h-8 rounded-full" src={src} alt={alt} /> */}
 					{/* <span className="font-bold ">{nickName}</span> */}
@@ -19,12 +46,11 @@ function MyCommentItem({ comment }) {
 						{comment}
 					</p>
 				</div>
-				<div className="flex gap-3">
-					<SmallButton>수정</SmallButton>
-					<SmallButton>삭제</SmallButton>
-				</div>
+			</Link>
+			<div className="flex gap-3">
+				<SmallButton onClick={handleDeletecomment}>삭제</SmallButton>
 			</div>
-		</>
+		</div>
 	);
 }
 
