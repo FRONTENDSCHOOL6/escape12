96f9c2b15b5d6fa3e38b3378ff0e173492170907
KIMGEUNDU <vlkoklv@naver.com>
2023-09-19 11:25:25 +0900

    [추가] 리액트쿼리, 머티리얼UI 추가 설치

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/index.css';
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<App />
+			<ReactQueryDevtools initialIsOpen />
+		</QueryClientProvider>
+	</React.StrictMode>
 );
