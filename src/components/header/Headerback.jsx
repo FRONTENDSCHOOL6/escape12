2f7be5c3444b65566662be83883784ee549dbbb4
JOSuYeoM <syjo1227@naver.com>
2023-09-19 21:38:48 +0900

    [수정] 다크모드 헤더 수정

--- src/components/header/Headerback.jsx
+++ src/components/header/Headerback.jsx
@@ -9,7 +9,7 @@ Headerback.propTypes = {
 function Headerback({ onClick = () => {}, children }) {
   return (
 		<header className="max-w-[600px] min-w-[320px] m-auto fixed top-0 left-0 right-0 py-3 text-center bg-light-ec1 dark:bg-dark-ec4 text-light-ec4 dark:text-dark-ec1 text-2xl font-bold z-10 flex items-center justify-center">
-			<button className="ml-2 absolute top-2 left-3" onClick={onClick}>
+			<button className="ml-2 absolute top-3 left-3" onClick={onClick}>
 				&lt;
 			</button>
 			<div className='flex items-center pl-5'>
