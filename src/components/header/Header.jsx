745df468aba91a439ca251de143c9ef61745010c
JOSuYeoM <syjo1227@naver.com>
2023-09-19 19:21:41 +0900

    [수정] 다크모드 구현

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ Header.propTypes = {
 
 function Header({ children }) {
 	return (
-		<header className="max-w-[600px] min-w-[320px] m-auto fixed top-0 left-0 right-0 py-3 text-center bg-light-ec1 dark:bg-dark-ec1 text-light-ec4 dark:text-dark-ec4 text-2xl font-bold z-10">
+		<header className="max-w-[600px] min-w-[320px] m-auto fixed top-0 left-0 right-0 py-3 text-center bg-light-ec1 dark:bg-dark-ec4 text-light-ec4 dark:text-dark-ec1 text-2xl font-bold z-10">
 			{children}
 			<ThemeToggleButton />
 		</header>
--- src/components/header/HeaderBackRecord.jsx
+++ src/components/header/HeaderBackRecord.jsx
@@ -15,7 +15,7 @@ function HeaderBackRecord({
 	pencilClick = null,
 }) {
 	return (
-		<header className="max-w-[600px] min-w-[320px] m-auto fixed top-0 left-0 right-0 py-3 text-center bg-light-ec1 dark:bg-dark-ec1 text-light-ec4 dark:text-dark-ec4 text-2xl font-bold z-10">
+		<header className="max-w-[600px] min-w-[320px] m-auto fixed top-0 left-0 right-0 py-3 text-center bg-light-ec1 dark:bg-dark-ec4 text-light-ec4 dark:text-dark-ec1 text-2xl font-bold z-10">
 			<div className="max-w-screen-md mx-auto flex items-center">
 				<button className="ml-2" onClick={onClick}>
 					&lt;
