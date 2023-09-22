ca6485b2c27c131cf6d225a02dcf6850ff717fac
JOSuYeoM <syjo1227@naver.com>
2023-09-22 16:44:41 +0900

    [수정] mypage 수정

---
+++ src/hooks/useMyPage.js
@@ -0,0 +1,27 @@
+import { getUserInfoFromStorage } from '@/api/getUserInfo';
+import pb from '@/api/pockethost';
+import { useQuery } from '@tanstack/react-query';
+
+const getMyPage = async () => {
+  const userUId = getUserInfoFromStorage();
+	const data = await pb
+		.collection('users')
+		.getOne(`${userUId?.model.id}`, {
+			expand: 'email',
+		});
+	return data;
+};
+
+const useMyPage = ({ keys = [] } = {}) => {
+	const queryKey = ['users', ...keys];
+
+	const queryData = useQuery({
+		queryKey,
+		queryFn: () => getMyPage(),
+		keepPreviousData: true,
+	});
+
+	return queryData;
+};
+
+export default useMyPage;
