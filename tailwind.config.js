cca64f21671b87a9aeba33f6f0198b9d3d1171f2
KIMGEUNDU <vlkoklv@naver.com>
2023-09-24 21:49:04 +0900

    [삭제] 주석 삭제

--- tailwind.config.js
+++ tailwind.config.js
@@ -58,7 +58,7 @@ export default {
 		extend: {
 			boxShadow: {
 				darkMode:
-					'0 10px 15px -3px rgba(0, 0, 0, .5), 0 4px 6px -2px rgba(0, 0, 0,.07)', // 커스텀 그림자 추가
+					'0 10px 15px -3px rgba(0, 0, 0, .5), 0 4px 6px -2px rgba(0, 0, 0,.07)',
 			},
 			keyframes: {
 				spinner: {
